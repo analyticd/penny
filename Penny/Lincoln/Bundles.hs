088f1b9511a259166319d84491cb5ed93fbaf615
Omari Norman <omari@smileystation.com>
2012-02-01 22:07:27 -0500

    WIP

---
+++ Penny/Lincoln/Bundles.hs
@@ -0,0 +1,57 @@
+module Penny.Lincoln.Boxes (
+  TransactionBox ( transaction, transactionMeta ),
+  PostingBox ( postingBundle, metaBundle ),
+  posting,
+  postingMeta,
+  transactionBox,
+  topLineMeta) where
+
+import Penny.Lincoln.Meta (TransactionMeta, unTransactionMeta)
+import Penny.Lincoln.Transaction (
+  Transaction, TopLine, Posting, unTransaction)
+import Penny.Lincoln.Family (children)
+import Penny.Lincoln.Family.Child (Child, child, parent)
+import Penny.Lincoln.Family.Siblings (flatten)
+import Penny.Lincoln.Family.Family ( Family )
+import qualified Penny.Lincoln.Family.Family as F
+
+data TransactionBox t p =
+  TransactionBox { transaction :: Transaction
+                 , transactionMeta :: (Maybe (TransactionMeta t p)) }
+  deriving Show
+
+data PostingBox t p =
+  PostingBox { postingBundle :: (Child TopLine Posting)
+             , metaBundle :: (Maybe (Child t p)) }
+  deriving Show
+
+transactionBox :: Transaction
+                  -> Maybe (TransactionMeta t p)
+                  -> TransactionBox t p
+transactionBox t mm = case mm of
+  Nothing -> TransactionBox t Nothing
+  (Just m) -> let
+    (ft, fm) = (unTransaction t, unTransactionMeta m)
+    len = length . F.children
+    in if len ft /= len fm
+       then error "Bundles.hs: error: metadata and transaction mismatch"
+       else TransactionBox t (Just m)
+
+postingBoxes :: [TransactionBox t p] -> [PostingBox t p]
+postingBoxes = concatMap toBox where
+  toBox (TransactionBox cTrans cMeta) = let
+    cs = flatten . children . unTransaction $ cTrans
+    metas = case cMeta of
+      Nothing -> repeat Nothing
+      (Just mb) -> map Just . flatten
+                   . children . unTransactionMeta $ mb
+    in zipWith PostingBox cs metas
+
+posting :: PostingBox t p -> Posting
+posting = child . postingBundle
+
+postingMeta :: PostingBox t p -> Maybe p
+postingMeta b = metaBundle b >>= return . child
+
+topLineMeta :: PostingBox t p -> Maybe t
+topLineMeta b = metaBundle b >>= return . parent
--- Penny/Lincoln/Meta.hs
+++ Penny/Lincoln/Meta.hs
@@ -1,9 +1,35 @@
-module Penny.Reports where
+module Penny.Lincoln.Meta where
+
+import qualified Penny.Lincoln.Bits as B
+import qualified Penny.Lincoln.Family.Family as F
+
+import qualified Data.Map as M
+import qualified Data.Text as X
 
 data Side = CommodityOnLeft | CommodityOnRight deriving Show
 data SpaceBetween = SpaceBetween | NoSpaceBetween deriving Show
 
-data CommodityFmt =
-  CommodityFmt { side :: Side
-               , between :: SpaceBetween }
+data Format =
+  Format { commodity :: B.Commodity
+         , side :: Side
+         , between :: SpaceBetween }
+  deriving Show
+
+newtype Line = Line { unLine :: Int }
+               deriving Show
+
+newtype Filename = Filename { unFilename :: X.Text }
+                   deriving Show
+
+class TopLineMeta a where
+  memoLine :: a -> Line
+  topLine :: a -> Line
+  filename :: a -> Filename
+
+class PostingMeta a where
+  line :: a -> Line
+  format :: a -> Maybe Format
+
+newtype TransactionMeta a b =
+  TransactionMeta { unTransactionMeta :: F.Family a b }
   deriving Show
