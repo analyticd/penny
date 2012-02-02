b6bcf2582772e78f69acdc504a1f945338254700
Omari Norman <omari@smileystation.com>
2012-02-01 22:23:10 -0500

    WIP

--- Penny/Lincoln/AccountMap.hs
+++ Penny/Lincoln/AccountMap.hs
@@ -1,4 +1,7 @@
 module Penny.Lincoln.AccountMap where
 
+import qualified Penny.Lincoln.Bits as B
 import qualified Data.NestedMap as N
 
+newtype AccountMap =
+  AccountMap (N.NestedMap B.SubAccountName DL.D
