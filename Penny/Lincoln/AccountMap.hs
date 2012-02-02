cf562cd2a7cc47bf4359a548555f106b4991a94b
Omari Norman <omari@smileystation.com>
2012-02-02 07:27:03 -0500

    remove AccountMap

--- Penny/Lincoln/AccountMap.hs
+++
@@ -1,7 +0,0 @@
-module Penny.Lincoln.AccountMap where
-
-import qualified Penny.Lincoln.Bits as B
-import qualified Data.NestedMap as N
-
-newtype AccountMap =
-  AccountMap (N.NestedMap B.SubAccountName DL.D
