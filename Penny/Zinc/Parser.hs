5d39f448698d2fa156c0925d40901f7a109dff8e
Omari Norman <omari@smileystation.com>
2012-02-08 07:59:59 -0500

    WIP

--- Penny/Zinc/Parser.hs
+++ Penny/Zinc/Parser.hs
@@ -2,8 +2,11 @@ module Penny.Zinc.Parser where
 
 import Control.Monad.Exception.Synchronous (
   Exceptional(Exception, Success))
+import Data.Char (toUpper)
+import Data.Map (Map)
+import qualified Data.Map as M
 import Data.Monoid (mempty, mappend)
-import Data.Monoid.Extra (Orderer(appOrderer))
+import Data.Monoid.Extra (Orderer(Orderer, appOrderer))
 import Data.Text (Text, pack, unpack)
 import System.Console.MultiArg.Combinator
   (mixedNoArg, mixedOneArg, longOneArg, longNoArg, longTwoArg)
@@ -18,6 +21,7 @@ import Penny.Copper.Qty (Radix, Separator, qty)
 
 import Penny.Copper.Meta ( TransactionMeta, PostingMeta )
 import qualified Penny.Lincoln.Predicates as P
+import qualified Penny.Lincoln.Queries as Q
 import Penny.Lincoln.Bits (DateTime, Qty)
 import Penny.Lincoln.Boxes (PostingBox)
 import qualified Penny.Zinc.Expressions as X
@@ -37,7 +41,8 @@ data State t p =
   State { sensitive :: M.CaseSensitive
         , matcher :: Text -> Exceptional Text (Text -> Bool)
         , tokens :: [X.Token (PostingBox t p -> Bool)]
-        , sorter :: Orderer (PostingBox t p -> Ordering) }
+        , sorter :: Orderer
+                    (PostingBox t p -> PostingBox t p -> Ordering) }
 
 blankState :: State t p
 blankState = State { sensitive = M.Insensitive
@@ -277,3 +282,46 @@ posix = changeState "posix" Nothing f where
 exact :: State t p -> ParserE Error (State t p)
 exact = changeState "exact" Nothing f where
   f st = st { matcher = \t -> return (M.exact (sensitive st) t) }
+
+--
+-- Sorting
+--
+ordering ::
+  (Ord r)
+  => (PostingBox t p -> r)
+  -> Orderer (PostingBox t p)
+ordering q = Orderer f where
+  f p1 p2 = compare (q p1) (q p2)
+
+flipOrder :: Orderer (PostingBox t p) -> Orderer (PostingBox t p)
+flipOrder (Orderer f) = Orderer f' where
+  f' p1 p2 = case f p1 p2 of
+    LT -> GT
+    GT -> LT
+    EQ -> EQ
+
+capitalizeFirstLetter :: String -> String
+capitalizeFirstLetter s = case s of
+  [] -> []
+  (x:xs) -> toUpper x : xs
+
+ords :: Map Text (Orderer (PostingBox t p))
+ords = M.fromList (lowers ++ uppers) where
+  uppers = map toReversed ordPairs
+  toReversed (s, f) =
+    (pack . capitalizeFirstLetter $ s, flipOrder f)
+  lowers = map toPair ordPairs
+  toPair (s, f) = (pack s, f)
+
+ordPairs :: [(String, Orderer (PostingBox t p))]
+ordPairs = 
+  [ ("payee", ordering Q.payee)
+  , ("date", ordering Q.dateTime)
+  , ("flag", ordering Q.flag)
+  , ("number", ordering Q.number)
+  , ("account", ordering Q.account)
+  , ("drCr", ordering Q.drCr)
+  , ("qty", ordering Q.qty)
+  , ("commodity", ordering Q.commodity)
+  , ("postingMemo", ordering Q.postingMemo)
+  , ("transactionMemo", ordering Q.transactionMemo) ]
