f9a06a64edca9e07fb5dd31372b9f50f33a1fefb
Omari Norman <omari@smileystation.com>
2012-02-08 08:00:26 -0500

    WIP

--- Penny/Zinc/Help.hs
+++ Penny/Zinc/Help.hs
@@ -112,7 +112,7 @@ help = unlines [
   "",
   "postings, pos",
   "  Show postings in order with a running balance.",
-  "  The register report takes the same options shown",
+  "  The postings report takes the same options shown",
   "  above; however, these options affect which postings",
   "  are shown in the report. Postings not shown still affect",
   "  the running balance.",
