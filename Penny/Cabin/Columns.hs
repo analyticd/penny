a9eea98f1c9ff65c807d2d6465d88218e87646fa
Omari Norman <omari@smileystation.com>
2012-02-09 16:37:07 -0500

    add columns

--- Penny/Cabin/Columns.hs
+++ Penny/Cabin/Columns.hs
@@ -1,5 +1,6 @@
 module Penny.Cabin.Columns where
 
+import Data.Monoid (Monoid)
 import Data.Text (Text)
 
 import Penny.Lincoln.Balance (Balance)
@@ -22,8 +23,32 @@ type CalculatorWithWidth t p m =
   -> [PriceBox m]
   -> [Text]
 
-data ColSpec t p m =
+data Justify = LJustify | RJustify
+
+data ColSize t p m =
   GrowToFit (Calculator t p m)
-  | Fixed Int (Calculator t p m)
+  | Fixed Columns (CalculatorWithWidth t p m)
   | Variable Double (CalculatorWithWidth t p m)
+
+data ColSpec t p m =
+  ColSpec (ColSize t p m) Justify
+
+data TableSpec t p m =
+  TableSpec [ColSpec t p m]
+
+data Phase1ResultItem t p m =
+  P1Done [Text]
+  | P1Variable Double (CalculatorWithWidth t p m)
+
+data Phase1ResultStatus t p m =
+  P1REmpty
+  | P1RWidth Int
+  | P1RVariable Double (CalculatorWithWidth t p m)
+
+data Phase1ResultRow t p m =
+  Phase1ResultRow [Phase1ResultItem t p m]
+
+data Phase1Table t p m =
+  Phase1Table [Phase1ResultRow t p m]
+
 
