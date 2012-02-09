21067dfa3a0fa938021cf6e5a467ef9f0483ea28
Omari Norman <omari@smileystation.com>
2012-02-09 18:50:09 -0500

    metadata conversion complete

--- Penny/Cabin/Columns.hs
+++ Penny/Cabin/Columns.hs
@@ -8,47 +8,47 @@ import Penny.Lincoln.Boxes (PostingBox, PriceBox)
 
 import Penny.Cabin.Class (Context, Columns)
 
-type Calculator t p m =
+type Calculator =
   Context
   -> Balance
-  -> PostingBox t p 
-  -> [PriceBox m]
+  -> PostingBox 
+  -> [PriceBox]
   -> [Text]
 
-type CalculatorWithWidth t p m =
+type CalculatorWithWidth =
   Columns
   -> Context
   -> Balance
-  -> PostingBox t p
-  -> [PriceBox m]
+  -> PostingBox
+  -> [PriceBox]
   -> [Text]
 
 data Justify = LJustify | RJustify
 
-data ColSize t p m =
-  GrowToFit (Calculator t p m)
-  | Fixed Columns (CalculatorWithWidth t p m)
-  | Variable Double (CalculatorWithWidth t p m)
+data ColSize =
+  GrowToFit Calculator
+  | Fixed Columns CalculatorWithWidth
+  | Variable Double CalculatorWithWidth
 
-data ColSpec t p m =
-  ColSpec (ColSize t p m) Justify
+data ColSpec =
+  ColSpec ColSize Justify
 
-data TableSpec t p m =
-  TableSpec [ColSpec t p m]
+data TableSpec =
+  TableSpec [ColSpec]
 
-data Phase1ResultItem t p m =
+data Phase1ResultItem =
   P1Done [Text]
-  | P1Variable Double (CalculatorWithWidth t p m)
+  | P1Variable Double (CalculatorWithWidth)
 
-data Phase1ResultStatus t p m =
+data Phase1ResultStatus =
   P1REmpty
   | P1RWidth Int
-  | P1RVariable Double (CalculatorWithWidth t p m)
+  | P1RVariable Double (CalculatorWithWidth)
 
-data Phase1ResultRow t p m =
-  Phase1ResultRow [Phase1ResultItem t p m]
+data Phase1ResultRow =
+  Phase1ResultRow [Phase1ResultItem]
 
-data Phase1Table t p m =
-  Phase1Table [Phase1ResultRow t p m]
+data Phase1Table =
+  Phase1Table [Phase1ResultRow]
 
 
