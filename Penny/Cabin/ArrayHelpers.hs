1c265743a050361394e67a9c5c2c06b29ac4de0e
Omari Norman <omari@smileystation.com>
2012-02-15 18:02:08 -0500

    WIP

--- Penny/Cabin/ArrayHelpers.hs
+++ Penny/Cabin/ArrayHelpers.hs
@@ -1,8 +1,15 @@
 module Penny.Cabin.ArrayHelpers where
 
+import Control.Applicative ((<$>), (<*>), pure)
 import Data.Array (Array, array, range, (!), Ix)
 import qualified Data.Array as A
+import qualified Data.Foldable as F
+import qualified Data.Traversable as T
 import Data.Word (Word)
+
+--data Table col row e = Table { unTable :: Array (col, row) e }
+--                     deriving Show
+
 
 -- | Given an index in a two-dimensional array, return all the values
 -- in that column. This function is partial. It will call error if the
@@ -34,6 +41,73 @@ row a r = let
   result = array (colMin, colMax) pairs
   valid = A.inRange (A.bounds a) (colMin, r)
   in if valid then result else error "row: given row not valid"
+
+newtype OneDim i e =
+  OneDim
+  { unOneDim :: Array i e }
+  deriving Show
+
+instance Ix i => Functor (OneDim i) where
+  fmap f (OneDim a) = OneDim $ fmap f a
+
+instance Ix i => F.Foldable (OneDim i) where
+  foldr f z (OneDim a) = foldr f z . A.elems $ a
+
+instance Ix i => T.Traversable (OneDim i) where
+  -- :: Applicative f
+  -- => OneDim (f a) -> f (OneDim a)
+  sequenceA (OneDim a) = let
+    mkT ls = OneDim arr where
+      arr = A.array (A.bounds a) es
+      es = zip (A.range . A.bounds $ a) ls
+    in mkT <$> T.sequenceA (A.elems a)
+
+newtype TRows col row e =
+  TRows { unTRows :: Array (col, row) e }
+  deriving Show
+
+instance (Ix col, Ix row) => Functor (TRows col row) where
+  fmap f (TRows a) = TRows $ fmap f a
+
+tRowsElems ::
+  (Ix col, Ix row)
+  => TRows col row e
+  -> [e]
+tRowsElems (TRows a) = map (a !) is where
+    ((minCol, minRow), (maxCol, maxRow)) = A.bounds a
+    is = flip (,)
+         <$> A.range (minRow, maxRow)
+         <*> A.range (minCol, maxCol)
+
+instance (Ix col, Ix row) => F.Foldable (TRows col row) where
+  foldr f z = foldr f z . tRowsElems
+
+instance (Ix col, Ix row) => T.Traversable (TRows col row) where
+  -- :: Applicative f
+  -- => TRows (f a) -> f (TRows a)
+  sequenceA trw@(TRows a) = let
+    es = tRowsElems trw
+    mkA ls = TRows arr where
+      arr = A.array (A.bounds a) els
+      els = zip (A.indices a) ls
+    in mkA <$> T.sequenceA es
+
+newtype TCols col row e =
+  TCols { unTCols :: Array (col, row) e }
+  deriving Show
+
+instance (Ix col, Ix row) => Functor (TCols col row) where
+  fmap f (TCols a) = TCols $ fmap f a
+
+instance (Ix col, Ix row) => F.Foldable (TCols col row) where
+  foldr f z = foldr f z . A.elems . unTCols
+
+instance (Ix col, Ix row) => T.Traversable (TCols col row) where
+  sequenceA (TCols a) = let
+    mkT ls = TCols $ A.array b es where
+      b = A.bounds a
+      es = zip (A.indices a) ls
+    in mkT <$> T.sequenceA (A.elems a)
 
 label ::
   (Ix col, Ix row)
