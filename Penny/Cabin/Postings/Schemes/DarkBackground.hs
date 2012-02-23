c3ed2023902158b0cff6cb4729cb652dcfdb5f6f
Omari Norman <omari@smileystation.com>
2012-02-22 23:02:07 -0500

    WIP

--- Penny/Cabin/Postings/Schemes/DarkBackground.hs
+++ Penny/Cabin/Postings/Schemes/DarkBackground.hs
@@ -22,36 +22,15 @@ evenBackground256 = CC.Default
 evenForeBack256 :: CC.ForeBack CC.Color256
 evenForeBack256 = CC.ForeBack evenForeground256 evenBackground256
 
-oddForeground8 :: CC.Color CC.Color8
-oddForeground8 = CC.Default
-
-oddBackground8 :: CC.Color CC.Color8
-oddBackground8 = CC.Default
-
-oddForeBack8 :: CC.ForeBack CC.Color8
-oddForeBack8 = CC.ForeBack oddForeground8 oddBackground8
-
-oddForeground256 :: CC.Color CC.Color256
-oddForeground256 = CC.Default
-
--- | Dark grey
-oddBackground256 :: CC.Color CC.Color256
-oddBackground256 = CC.Color (CC.color256 235)
-
-oddForeBack256 :: CC.ForeBack CC.Color256
-oddForeBack256 = CC.ForeBack oddForeground256 oddBackground256
-
 evenBaseColorSet :: CC.ColorSet
 evenBaseColorSet = CC.ColorSet evenForeBack8 evenForeBack256
-
-oddBaseColorSet :: CC.ColorSet
-oddBaseColorSet = CC.ColorSet oddForeBack8 oddForeBack256
 
 evenTextSpec :: CC.TextSpec
 evenTextSpec = CC.defaultSpec { CC.colorSet = evenBaseColorSet }
 
 oddTextSpec :: CC.TextSpec
-oddTextSpec = CC.defaultSpec { CC.colorSet = oddBaseColorSet }
+oddTextSpec = S.switchBackground CC.Default
+              (CC.Color (CC.color256 235)) evenTextSpec
 
 baseColors :: PC.BaseColors
 baseColors = PC.BaseColors evenTextSpec oddTextSpec
