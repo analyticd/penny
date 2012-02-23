9a256b414cf881fc871d94d481ef4bbc0dd19a5f
Omari Norman <omari@smileystation.com>
2012-02-23 08:58:21 -0500

    WIP

--- Penny/Cabin/Postings/Schemes/DarkBackground.hs
+++ Penny/Cabin/Postings/Schemes/DarkBackground.hs
@@ -4,36 +4,16 @@ import qualified Penny.Cabin.Postings.Colors as PC
 import qualified Penny.Cabin.Colors as CC
 import qualified Penny.Cabin.Colors.Switch as S
 
-
-
-evenForeground8 :: CC.Color CC.Color8
-evenForeground8 = CC.Default
-
-evenBackground8 :: CC.Color CC.Color8
-evenBackground8 = CC.Default
-
-evenForeBack8 :: CC.ForeBack CC.Color8
-evenForeBack8 = CC.ForeBack evenForeground8 evenBackground8
-
-evenForeground256 :: CC.Color CC.Color256
-evenForeground256 = CC.Default
-
-evenBackground256 :: CC.Color CC.Color256
-evenBackground256 = CC.Default
-
-evenForeBack256 :: CC.ForeBack CC.Color256
-evenForeBack256 = CC.ForeBack evenForeground256 evenBackground256
-
-evenBaseColorSet :: CC.ColorSet
-evenBaseColorSet = CC.ColorSet evenForeBack8 evenForeBack256
-
 evenTextSpec :: CC.TextSpec
-evenTextSpec = CC.defaultSpec { CC.colorSet = evenBaseColorSet }
+evenTextSpec = CC.defaultSpec
 
 oddTextSpec :: CC.TextSpec
 oddTextSpec = S.switchBackground CC.Default
               (CC.Color (CC.color256 235)) evenTextSpec
-
+              
 baseColors :: PC.BaseColors
 baseColors = PC.BaseColors evenTextSpec oddTextSpec
 
+evenDebit :: CC.TextSpec
+evenDebit = S.switchForeground (CC.Color CC.Magenta)
+            (CC.Color (CC.color256 13)) evenTextSpec
