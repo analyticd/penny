3f3e9bbfed74ecb4d3fb11ec6a1b149df6e1c2b4
Omari Norman <omari@smileystation.com>
2012-02-24 08:06:40 -0500

    WIP

--- Penny/Cabin/Postings/Options.hs
+++ Penny/Cabin/Postings/Options.hs
@@ -4,6 +4,7 @@ import qualified Data.Text as X
 import Data.Time (formatTime)
 import System.Locale (defaultTimeLocale)
 import qualified Data.Time as Time
+import System.Environment (getEnvironment)
 
 import qualified Penny.Lincoln.Balance as Bal
 import qualified Penny.Lincoln.Bits as Bits
@@ -11,6 +12,7 @@ import qualified Penny.Lincoln.Queries as Q
 import qualified Penny.Cabin.Allocate as A
 import qualified Penny.Cabin.Postings.Colors as C
 import qualified Penny.Cabin.Postings.Types as T
+import qualified Penny.Cabin.Postings.Schemes.DarkBackground as Dark
 
 data Options =
   Options { drCrColors :: C.DrCrColors
@@ -26,9 +28,47 @@ data Options =
 newtype ReportWidth = ReportWidth { unReportWidth :: Int }
                       deriving (Eq, Show, Ord)
 
-defaultDateFormat :: T.PostingInfo -> X.Text
-defaultDateFormat p = X.pack (formatTime defaultTimeLocale fmt d) where
+ymd :: T.PostingInfo -> X.Text
+ymd p = X.pack (formatTime defaultTimeLocale fmt d) where
   d = Time.utctDay . Bits.unDateTime . Q.dateTime . T.postingBox $ p
   fmt = "%Y-%m-%d"
 
+qtyAsIs :: T.PostingInfo -> X.Text
+qtyAsIs p = X.pack . show . Bits.unQty . Q.qty . T.postingBox $ p
 
+balanceAsIs :: Bits.Commodity -> Bal.Nought -> X.Text
+balanceAsIs _ n = case n of
+  Bal.Zero -> X.pack "--"
+  Bal.NonZero c -> X.pack . show . Bits.unQty . Bal.qty $ c
+
+columnsVar :: IO (Maybe String)
+columnsVar = getEnvironment >>= return . lookup "COLUMNS"
+  
+defaultWidth :: ReportWidth
+defaultWidth = ReportWidth 80
+
+columnsVarToWidth :: Maybe String -> ReportWidth
+columnsVarToWidth ms = case ms of
+  Nothing -> defaultWidth
+  Just str -> case reads str of
+    [] -> defaultWidth
+    (i, []):[] -> if i > 0 then ReportWidth i else defaultWidth
+    _ -> defaultWidth
+
+defaultOptions :: ReportWidth -> Options
+defaultOptions rw =
+  Options { drCrColors = Dark.drCrColors
+          , baseColors = Dark.baseColors
+          , dateFormat = ymd
+          , qtyFormat = qtyAsIs
+          , balanceFormat = balanceAsIs
+          , payeeAllocation = A.allocation 40
+          , accountAllocation = A.allocation 60
+          , width = rw
+          , subAccountLength = 2 }
+
+getOptions :: IO Options
+getOptions = do
+  c <- columnsVar
+  let w = columnsVarToWidth c
+  return $ defaultOptions w
