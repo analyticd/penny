58ee33f07863bb8f92669287f6a21d4377c023c2
Omari Norman <omari@smileystation.com>
2012-02-25 13:41:24 -0500

    WIP

--- Penny/Zinc/Ledgers.hs
+++ Penny/Zinc/Ledgers.hs
@@ -1,2 +1,25 @@
 module Penny.Zinc.Ledgers where
 
+import Control.Applicative ((<$>), many, pure, (<*>), liftA)
+import qualified Data.List.NonEmpty as NE
+import Data.Text (Text, pack)
+import qualified Penny.Liberty.Error as E
+import System.Console.MultiArg.Combinator (option)
+import System.Console.MultiArg.Prim (ParserE, nextArg, try)
+
+data Filename =
+  Filename Text
+  | Stdin
+
+filename :: ParserE E.Error Filename
+filename = do
+  a <- nextArg
+  return $ if a == pack "-"
+           then Stdin
+           else Filename a
+
+filenames :: ParserE E.Error (NE.NonEmpty Filename)
+filenames = do
+  fn1 <- option Stdin (Filename <$> nextArg)
+  fns <- option [] $ liftA (fmap Filename) (many nextArg)
+  return $ NE.nonEmpty fn1 fns
---
+++ Penny/Zinc/Parser.hs
@@ -0,0 +1,7 @@
+module Penny.Zinc.Parser where
+
+import System.Console.MultiArg.Prim (ParserE)
+import qualified Penny.Liberty.Types as T
+import qualified Penny.Cabin.Class as C
+import qualified Penny.Liberty.Error as E
+import qualified Penny.Zinc.Filter as F
