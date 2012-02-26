2b94c535dc34c87cd03f603cfb78d11f670a7ce4
Omari Norman <omari@smileystation.com>
2012-02-25 19:58:06 -0500

    WIP

--- Penny.hs
+++ Penny.hs
@@ -3,15 +3,39 @@
 Penny is organized into a tree of modules, each with a name. Check out
 the links for details on each component of Penny.
 
-"Penny.Cabin" - Penny reports
+"Penny.Cabin" - Penny reports. Depends on Lincoln and Liberty.
 
-"Penny.Copper" - the Penny parser
+"Penny.Copper" - the Penny parser. Depends on Lincoln.
 
 "Penny.Denver" - Penny testing. Only available if you built Penny with
 the @testing@ flag; clicking on this link if you did not use the @testing@ flag will get you a @file not found@ error.
 
-"Penny.Lincoln" - the Penny core
+"Penny.Liberty" - Penny command line parser helpers. Depends on Lincoln and Copper.
 
-"Penny.Zinc" - the Penny command-line interface
+"Penny.Lincoln" - the Penny core. Depends on no other Penny components.
+
+"Penny.Zinc" - the Penny command-line interface. Depends on Cabin, Copper, Liberty, and Lincoln.
+
+A crude drawing of these dependencies appears in the source code for this module (I don't know if Haddock would like it.)
+
+-}
+
+{-
+
+Graph:
+                     +----------Zinc
+                     |           |
+                     |           |
+                     |    +----Cabin
+                     |    |      |
+                     |    |      |
+                     |----+-> Liberty----+
+                     |    |      |       |
+                     |    |      |       |
+                     |----+-> Copper     |
+                     |    |      |       |
+                     |    |      |       |
+                     +----+-> Lincoln <--+
+
 -}
 module Penny where
