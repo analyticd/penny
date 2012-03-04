53e37a8c73cfaa1f57c7e53d7c75c489354b6ab5
Omari Norman <omari@smileystation.com>
2012-03-04 13:30:03 -0500

    WIP

--- Penny/Copper/Qty.hs
+++ Penny/Copper/Qty.hs
@@ -1,21 +1,17 @@
--- | Quantity parsers. There are two kinds of quantity parsers:
---
--- * Level 1 quantity. Can include spaces, if the grouping character
--- is a space. However these must be quoted when in a Ledger file
--- (from the command line they need not be quoted). The quote
--- character is a caret, @^@.
---
--- * Level 2 quantity. These include no spaces. They need not be
--- quoted.
---
--- A BNF specification for quantities appears in the source code
--- comments.
-module Penny.Copper.Qty where
+module Penny.Copper.Qty (
+  -- * Setting the radix and separator characters
+  RadGroup, periodComma, periodSpace, commaPeriod,
+  commaSpace,
+  
+  -- * Parsing quantities
+  qtyUnquoted,
+  qtyQuoted) where
 
 import Control.Applicative ((<$>), (<*>), (<$), (*>), optional)
+import qualified Data.Decimal as D
 import qualified Data.List.NonEmpty as NE
 import Text.Parsec ( char, (<|>), many, many1, try, (<?>), 
-                     sepBy1, digit)
+                     sepBy1, digit, between)
 import qualified Text.Parsec as P
 import Text.Parsec.Text ( Parser )
 
@@ -71,30 +67,98 @@ parseGrouper g = () <$ char c <?> "grouping character" where
              | <radix> <fractional>
 -}
 
-groupedDigits :: Grouper -> Parser String
-groupedDigits g = concat
-            <$> sepBy1 (many1 P.digit) (parseGrouper g)
-
-whole :: Grouper -> Parser String
-whole g = p <$> group1 <*> optional groupRest <?> "whole number" where
+wholeGrouped :: Grouper -> Parser String
+wholeGrouped g = p <$> group1 <*> optional groupRest <?> e where 
+  e = "whole number"
   group1 = many1 digit
   groupRest = parseGrouper g *> sepBy1 (many1 digit) (parseGrouper g)
   p g1 gr = case gr of
     Nothing -> g1
     (Just groups) -> g1 ++ (concat groups)
 
+wholeNonGrouped :: Parser String
+wholeNonGrouped = many1 digit
+
 fractional :: Parser String
 fractional = many1 digit
 
-data Number = Whole String
-              | WholeRad String
-              | WholeRadFrac String String
-              | RadFrac String
+data NumberStr = Whole String
+               | WholeRad String
+               | WholeRadFrac String String
+               | RadFrac String
 
-number :: Parser Number
+startsRad :: Radix -> Parser (Maybe String)
+startsRad r = parseRadix r *> (optional (many1 P.digit))
 
-lvl1QtyUnquoted :: Parser Qty
-lvl1QtyUnquoted = undefined
+fractionalOnly :: Radix -> Parser String
+fractionalOnly r = parseRadix r *> many1 P.digit
+
+numberStrGrouped :: Radix -> Grouper -> Parser NumberStr
+numberStrGrouped r g = startsWhole <|> fracOnly <?> e where
+  e = "quantity, with optional grouping"
+  startsWhole = p <?> "whole number" where
+    p = do
+      wholeStr <- wholeGrouped g
+      mayRad <- optional (parseRadix r)
+      case mayRad of
+        Nothing -> return $ Whole wholeStr
+        Just _ -> do
+          mayFrac <- optional $ many1 P.digit
+          case mayFrac of
+            Nothing -> return $ WholeRad wholeStr
+            Just frac -> return $ WholeRadFrac wholeStr frac
+  fracOnly = RadFrac <$> fractionalOnly r
+
+numberStrNonGrouped :: Radix -> Parser NumberStr
+numberStrNonGrouped r = startsWhole <|> fracOnly <?> e where
+  e = "quantity, no grouping"
+  startsWhole = p <?> "whole number" where
+    p = do
+      wholeStr <- wholeNonGrouped
+      mayRad <- optional (parseRadix r)
+      case mayRad of
+        Nothing -> return $ Whole wholeStr
+        Just _ -> do
+          mayFrac <- optional $ many1 P.digit
+          case mayFrac of
+            Nothing -> return $ WholeRad wholeStr
+            Just frac -> return $ WholeRadFrac wholeStr frac
+  fracOnly = RadFrac <$> fractionalOnly r
+
+toDecimal :: NumberStr -> D.Decimal
+toDecimal s = read d where
+  d = case s of
+    Whole str -> str
+    WholeRad str -> str
+    WholeRadFrac wh fr -> wh ++ "." ++ fr
+    RadFrac fr -> '.':fr
+
+-- | Unquoted quantity. These include no spaces, regardless of what
+-- the grouping character is.
+qtyUnquoted :: RadGroup -> Parser Qty
+qtyUnquoted (RadGroup r g) = f <$> p where
+  f = (partialNewQty . toDecimal)
+  p = case g of
+    GSpace -> numberStrNonGrouped r
+    _ -> numberStrGrouped r g
+
+-- | Parse quoted quantity. It can include spaces, if the grouping
+-- character is a space. However these must be quoted when in a Ledger
+-- file (from the command line they need not be quoted). The quote
+-- character is a caret, @^@.
+qtyQuoted :: RadGroup -> Parser Qty
+qtyQuoted (RadGroup r g) = between (char '^') (char '^') p where
+  p = (partialNewQty . toDecimal) <$> numberStrGrouped r g
+
+{-
+numberStr :: Radix -> Separator -> Parser NumberStr
+numberStr r s = startsWhole <|> fractionalOnly <?> e where
+  e = "quantity"
+  startsWhole = f <$> whole <*> startsRad r where
+    f sWhole mayFrac = case mayFrac of
+      Nothing -> Whole sWhole
+      Just frac -> Whole
+-}
 
 {-
 newtype Radix = Radix { unRadix :: Char }
