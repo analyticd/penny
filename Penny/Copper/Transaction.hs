32698e7429ca679df80c687204f35cf223d14925
Omari Norman <omari@smileystation.com>
2012-03-05 08:58:30 -0500

    WIP

--- Penny/Copper/Transaction.hs
+++ Penny/Copper/Transaction.hs
@@ -1,5 +1,6 @@
 module Penny.Copper.Transaction where
 
+import Control.Applicative ((<$>), (<*>))
 import qualified Control.Monad.Exception.Synchronous as Ex
 import Text.Parsec (try, many )
 import Text.Parsec.Text ( Parser )
@@ -13,12 +14,45 @@ import Penny.Lincoln.Family.Family ( Family ( Family ) )
 import Penny.Lincoln.Meta (TransactionMeta(TransactionMeta))
 import Penny.Lincoln.Boxes (transactionBox, TransactionBox)
 import qualified Penny.Lincoln.Transaction as T
+import qualified Penny.Lincoln.Transaction.Unverified as U
 
 errorStr :: T.Error -> String
 errorStr e = case e of
   T.UnbalancedError -> "postings are not balanced"
   T.TooManyInferError -> "too many postings with entry amounts to infer"
   T.CouldNotInferError -> "could not infer entry for posting"
+
+mkTransaction ::
+  M.Filename
+  -> DT.DefaultTimeZone
+  -> Qt.RadGroup
+  -> (U.TopLine, M.TopLineLine, Maybe M.TopMemoLine)
+  -> (U.Posting, M.PostingMeta)
+  -> (U.Posting, M.PostingMeta)
+  -> [(U.Posting, M.PostingMeta)]
+  -> Ex.Exceptional String TransactionBox
+mkTransaction fn dtz rg tripTop p1 p2 ps = let
+  (tl, tll, tml) = tripTop
+  famTrans = Family tl (fst p1) (fst p2) (map fst ps)
+  paMeta = M.TopLineMeta tml (Just tll) (Just fn)
+  famMeta = Family paMeta (snd p1) (snd p2) (map snd ps)
+  meta = Just . TransactionMeta $ famMeta
+  errXact = T.transaction famTrans
+  in case errXact of
+    Ex.Exception err -> Ex.Exception . errorStr $ err
+    Ex.Success x -> return (transactionBox x meta)
+
+maybeTransaction ::
+  M.Filename
+  -> DT.DefaultTimeZone
+  -> Qt.RadGroup
+  -> Parser (Ex.Exceptional String TransactionBox)
+maybeTransaction fn dtz rg =
+  mkTransaction fn dtz rg
+  <$> topLine dtz
+  <*> Po.posting rg
+  <*> Po.posting rg
+  <*> many (Po.posting rg)
 
 {-
 transaction ::
