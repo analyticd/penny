79c4ba073a2f48cf5693bfa2c5967934d0df9976
Omari Norman <omari@smileystation.com>
2012-03-04 20:48:13 -0500

    WIP

--- Penny/Copper/Memos/Posting.hs
+++ Penny/Copper/Memos/Posting.hs
@@ -1,33 +1,29 @@
 module Penny.Copper.Memos.Posting where
 
+import Control.Applicative ((<$>), (<*>), (*>), (<*))
+import qualified Data.Char as C
 import Control.Monad ( liftM, void, when )
 import Data.Text ( pack )
 import Text.Parsec (
-  try, many1, char, getParserState, 
-  sourceColumn, statePos, noneOf, manyTill, many )
+  try, many1, char, getParserState, satisfy,
+  sourceColumn, statePos, noneOf, manyTill, many, (<?>))
 import Text.Parsec.Text ( Parser )
 
+import Penny.Copper.Util (inCat)
 import qualified Penny.Lincoln.Meta as M
 import qualified Penny.Lincoln.Bits as B
 import Penny.Lincoln.TextNonEmpty ( TextNonEmpty ( TextNonEmpty ) )
 
-memo :: M.Column
-        -> Parser B.Memo
-memo col = do
-  (c:cs) <- liftM concat (many1 (try (postingMemoLine col)))
-  return . B.Memo $ TextNonEmpty c (pack cs)
 
-postingMemoLine ::
-  M.Column
-  -- ^ Column that the posting line started at
-  -> Parser String
-postingMemoLine (M.Column aboveCol) = do
-  void (many (char ' '))
-  st <- getParserState
-  let currCol = sourceColumn . statePos $ st
-  when (currCol <= aboveCol) $
-    fail $ "memo line is not indented farther than corresponding "
-    ++ "posting line"
-  c <- noneOf "\t\n"
-  cs <- manyTill (noneOf "\t\n") (char '\n')
-  return (c : (cs ++ "\n"))
+isCommentChar :: Char -> Bool
+isCommentChar c = inCat C.UppercaseLetter C.OtherSymbol c
+                  || c == ' '
+
+memo :: Parser B.Memo
+memo = let
+  p1 = satisfy isCommentChar
+  ps = many (satisfy isCommentChar)
+  f c cs = B.Memo $ TextNonEmpty c (pack cs)
+  in char '\''
+     *> (f <$> p1 <*> ps <?> "posting memo")
+     <* char '\n'
--- Penny/Copper/Memos/Transaction.hs
+++ Penny/Copper/Memos/Transaction.hs
@@ -1,19 +1,25 @@
 module Penny.Copper.Memos.Transaction where
 
 import Control.Monad ( void, liftM )
+import qualified Data.Char as C
 import Data.Text ( pack )
 import Text.Parsec (
   char, many1, satisfy, sourceLine, getPosition )
 import Text.Parsec.Text ( Parser )
 
+import Penny.Copper.Util (inCat)
 import Penny.Lincoln.Meta (TopMemoLine(TopMemoLine), Line(Line))
 import qualified Penny.Lincoln.Bits as B
 import Penny.Lincoln.TextNonEmpty ( TextNonEmpty ( TextNonEmpty ) )
 
+isCommentChar :: Char -> Bool
+isCommentChar c = inCat C.UppercaseLetter C.OtherSymbol c
+                  || c == ' '
+
 line :: Parser String
 line = do
   void $ char ';'
-  cs <- many1 (satisfy (/= '\n'))
+  cs <- many1 (satisfy isCommentChar)
   void $ char '\n'
   return (cs ++ "\n")
 
