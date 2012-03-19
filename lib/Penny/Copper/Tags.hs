cad1e170f8026e6ac392e557b92b37457acd27ff
Omari Norman <omari@smileystation.com>
2012-03-19 08:15:02 -0400

    WIP

--- lib/Penny/Copper/Tags.hs
+++ lib/Penny/Copper/Tags.hs
@@ -1,14 +1,14 @@
-module Penny.Copper.Tags where
+module Penny.Copper.Tags (isTagChar, tags) where
 
 import Control.Applicative ((<$>), (*>), (<*>))
 import qualified Data.Char as C
-import Data.Text ( pack )
+import qualified Data.Text as X
 import Text.Parsec (char, satisfy, many, (<?>))
 import Text.Parsec.Text ( Parser )
 
 import Penny.Copper.Util (inCat, lexeme)
 import qualified Penny.Lincoln.Bits as B
-import Penny.Lincoln.TextNonEmpty ( TextNonEmpty ( TextNonEmpty ) )
+import qualified Penny.Lincoln.TextNonEmpty as TNE
 
 isTagChar :: Char -> Bool
 isTagChar = inCat C.UppercaseLetter C.OtherNumber
@@ -18,10 +18,18 @@ tagChar = satisfy isTagChar
 
 tag :: Parser B.Tag
 tag = (char '#' *> (f <$> tagChar <*> many tagChar)) <?> e where
-  f t ts = B.Tag $ TextNonEmpty t (pack ts)
+  f t ts = B.Tag $ TNE.TextNonEmpty t (X.pack ts)
   e = "tag"
 
 tags :: Parser B.Tags
 tags = B.Tags <$> many (lexeme tag)
 
+renderTag :: B.Tag -> Maybe X.Text
+renderTag (B.Tag t) =
+  if TNE.all isTagChar t
+  then Just $ X.cons '#' (TNE.toText t)
+  else Nothing
 
+render :: B.Tags -> Maybe X.Text
+render (B.Tags ts) = do
+  
