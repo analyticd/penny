f4ab7e2dca0449088843de4ac689b5ef8cd380ff
Omari Norman <omari@smileystation.com>
2012-07-05 09:05:57 -0400

    WIP

--- lib/Penny/Lincoln/Serial.hs
+++ lib/Penny/Lincoln/Serial.hs
@@ -1,8 +1,10 @@
 module Penny.Lincoln.Serial (
   Serial, forward, backward, serials,
-  serialItems, serialChildren, serialParents) where
+  serialItems, serialChildrenInFamilies, serialParentsInFamilies,
+  selectiveSerialParents) where
 
 import Data.Foldable (foldl', toList)
+import qualified Data.Either as E
 import qualified Penny.Lincoln.Family as F
 import qualified Control.Monad.Trans.State as St
 
@@ -36,7 +38,7 @@ serials as = zipWith Serial fs rs where
 
 
 -- | Label a list of parents with serials, in order.
-serialParents ::
+serialParentsInFamilies ::
   (Serial -> pOld -> pNew)
   -- ^ Function that takes the serial and the old parent, and returns
   -- a new parent.
@@ -44,7 +46,7 @@ serialParents ::
   -> [F.Family pOld c]
   -> [F.Family pNew c]
 
-serialParents f ls =
+serialParentsInFamilies f ls =
   let nf = 0
       nb = (length ls) - 1
       initState = (nf, nb)
@@ -56,35 +58,92 @@ serialParents f ls =
   in St.evalState (mapM (F.mapParentM g) ls) initState
 
 -- | Label a list of children with serials, in order.
-serialChildren ::
+serialChildrenInFamilies ::
   (Serial -> cOld -> cNew)
   -- ^ Function that takes the serial and the old child, and returns a
   -- new child.
   
   -> [F.Family p cOld]
   -> [F.Family p cNew]
-serialChildren f ls = 
+serialChildrenInFamilies f ls = 
   let nf = 0
       len = length . concatMap toList . map F.orphans $ ls
       nb = len - 1
       initState = (nf, nb)
-  in St.evalState (mapM (serialFam f) ls) initState
+  in St.evalState (mapM (serialChildrenInFamily f) ls) initState
 
-serialFam ::
+serialChildrenInFamily ::
   (Serial -> cOld -> cNew)
   -> F.Family p cOld
   -> St.State (NextFwd, NextBack) (F.Family p cNew)
-serialFam f = F.mapChildrenM (serialItem f)
+serialChildrenInFamily f = F.mapChildrenM (serialItemSt f)
 
 type NextFwd = Int
 type NextBack = Int
 
-serialItem ::
+serialItemSt ::
   (Serial -> cOld -> cNew)
   -> cOld
   -> St.State (NextFwd, NextBack) cNew
-serialItem f old = do
+serialItemSt f old = do
   (fwd, bak) <- St.get
   let s = Serial fwd bak
   St.put (succ fwd, pred bak)
   return (f s old)
+
+
+selectiveSerialParents ::
+  (Serial -> p -> p')
+  -> (a -> Either (F.Family p c, F.Family p' c -> b) b)
+  -> [a]
+  -> [b]
+selectiveSerialParents xformer selector as =
+  let nf = 0
+      fams = map fst . E.lefts . map selector $ as
+      len = length fams
+      initState = (0, len - 1)
+      k = selectiveSerialParentsSt xformer selector
+  in St.evalState (mapM k as) initState
+      
+
+selectiveSerialParentsSt ::
+  (Serial -> p -> p')
+  -> (a -> Either (F.Family p c, F.Family p' c -> b) b)
+  -> a
+  -> St.State (NextFwd, NextBack) b
+selectiveSerialParentsSt xformer selector a =
+  case selector a of
+    Left ((F.Family p c1 c2 cs), toB) -> do
+      (fwd, bak) <- St.get
+      let s = Serial fwd bak
+          fam = F.Family (xformer s p) c1 c2 cs
+          b = toB fam
+      St.put (succ fwd, pred bak)
+      return b
+    Right b -> return b
+
+selectiveSerialChildren ::
+  (Serial -> c -> c')
+  -> (a -> Either (F.Family p c, F.Family p c' -> b) b)
+  -> [a]
+  -> [b]
+selectiveSerialChildren xformer selector as =
+  let nf = 0
+      fams = map fst . E.lefts . map selector $ as
+      len = length . concatMap toList . map F.orphans $ fams
+      initState = (nf, len - 1)
+      k = selectiveSerialChildrenSt xformer selector
+  in St.evalState (mapM k as) initState
+
+
+selectiveSerialChildrenSt ::
+  (Serial -> c -> c')
+  -> (a -> Either (F.Family p c, F.Family p c' -> b) b)
+  -> a
+  -> St.State (NextFwd, NextBack) b
+selectiveSerialChildrenSt xformer selector a =
+  case selector a of
+    Left (fam, toB) -> do
+      fam' <- serialChildrenInFamily xformer fam
+      return $ toB fam'
+    Right b -> return b
