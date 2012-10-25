2bb94c102a3b74a2f7ba6ac773b08cb27ae07c2e
Omari Norman <omari@smileystation.com>
2012-10-25 06:28:42 -0400

    Selloff additions

--- bin/penny-selloff.hs
+++ bin/penny-selloff.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Control.Monad (guard)
+import qualified Penny.Copper as Cop
+import Penny.Copper.DateTime (dateTime)
 import qualified Penny.Lincoln as L
 import qualified Control.Monad.Exception.Synchronous as Ex
 import Data.List.NonEmpty (NonEmpty((:|)))
@@ -8,6 +10,8 @@ import Data.List (find)
 import qualified Data.Map as M
 import Data.Maybe (mapMaybe)
 import Data.Text (pack)
+import qualified Text.Parsec as Parsec
+
 
 -- | A selloff pertains only to commodities that are in a particular
 -- group, where the group is indicated in the account name. The group
@@ -74,27 +78,33 @@ cmdtyCurrency (ProceedsAcct a) ls = do
 basisSub :: L.SubAccountName
 basisSub = L.SubAccountName (L.TextNonEmpty 'B' (pack "asis"))
 
-data BasisInfo = BasisInfo
-  { basisLabel :: SelloffLabel
-  , basisDate :: L.DateTime
-  } deriving Show
-
 -- | Examines an account to determine whether it is a Basis
--- account. Returns True if it is, False if it is not, and an error if
--- the account matches Basis:SelloffLabel but does not have exactly
--- one additional sub-account. Does not check the sub-account after
--- the selloff label to ensure that it is a proper DateTime.
-isBasisAccount :: SelloffLabel -> L.Account -> Maybe Bool
+-- account and has the selloff label we're interested in.
+isBasisAccount :: SelloffLabel -> L.Account -> Bool
 isBasisAccount (SelloffLabel l) (L.Account (s1 :| sr))
   | s1 == basisSub = case sr of
-      [] -> Just False
-      s:ls ->
-        if l == s
-        then case ls of
-          _:[] -> Just True
-          _ -> Nothing
-        else Just False
-  | otherwise = Just False
+      [] -> False
+      s:_ -> l == s
+  | otherwise = False
+
+-- | Examines an Account to see if it has a valid DateTime as the
+-- third sub-account, and to ensure that there are no additional
+-- sub-accounts. If it is valid, returns the DateTime; fails
+-- otherwise.
+basisDateTime :: Cop.DefaultTimeZone -> L.Account -> Maybe L.DateTime
+basisDateTime dtz (L.Account (_ :| (_ : d : []))) =
+  case Parsec.parse (dateTime dtz) "" (L.text d) of
+    Left _ -> Nothing
+    Right dt -> Just dt
+basisDateTime _ _ = Nothing
+
+-- | Information on a single Basis corresponding to an asset purchase.
+data Basis = Basis
+  { basisCurrencyQty :: L.Qty
+  , basisStockQty :: L.Qty
+  } deriving Show
+
+-- | Gets the Basis information pertaining to a single
 
 main :: IO ()
 main = undefined
