3b9b26be9485156576633f2e9f3feb9e1089bfbd
Omari Norman <omari@smileystation.com>
2012-10-26 07:03:32 -0400

    add tests

--- tests/PennyTest/Penny/Lincoln/Bits/Qty.hs
+++ tests/PennyTest/Penny/Lincoln/Bits/Qty.hs
@@ -2,12 +2,15 @@
 
 module PennyTest.Penny.Lincoln.Bits.Qty where
 
+import qualified Control.Monad.Exception.Synchronous as Ex
+import Control.Monad.Trans.Class (lift)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Control.Applicative ((<$>), (<*>))
 import qualified Test.Framework as TF
 import qualified Test.QuickCheck as QC
 import qualified Test.QuickCheck.Gen as G
 import qualified Data.Decimal as D
+import qualified Penny.Lincoln.Bits.Qty as Q
 
 tests :: TF.Test
 tests = TF.testGroup "PennyTest.Penny.Lincoln.Bits.Qty"
@@ -29,3 +32,24 @@ typicalDecimal = G.sized $ \s ->
   <$> G.choose (0, 5)
   <*> G.choose (1, 2 ^ s)
 
+genQty
+  :: (G.Gen D.Decimal)
+  -> Ex.ExceptionalT QC.Result Gen Q.Qty
+genQty g = do
+  d <- lift g
+  case Q.newQty d of
+    Nothing -> Ex.throwT (QC.failed { QC.reason = r })
+      where
+        r = "Qty generation failed from decimal " ++ show d
+    Just q -> return q
+
+{-
+-- | > (x + y - y) == x
+prop_addSubt :: QC.Gen QC.Result
+prop_addSubt = do
+  x <- typicalDecimal
+  y <- typicalDecimal
+  case (x `Qty.add` y) `Qty.subt` y of
+    Nothing -> return QC.failed
+    Just r -> if r ==
+-}
