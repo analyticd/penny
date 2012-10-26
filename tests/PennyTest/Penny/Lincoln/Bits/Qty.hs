802912fce6bdf10c216ff2f16f4425a0c1e7e4ec
Omari Norman <omari@smileystation.com>
2012-10-26 07:23:03 -0400

    add tests

--- tests/PennyTest/Penny/Lincoln/Bits/Qty.hs
+++ tests/PennyTest/Penny/Lincoln/Bits/Qty.hs
@@ -9,12 +9,13 @@ import Control.Applicative ((<$>), (<*>))
 import qualified Test.Framework as TF
 import qualified Test.QuickCheck as QC
 import qualified Test.QuickCheck.Gen as G
+import qualified Test.QuickCheck.Property as P
 import qualified Data.Decimal as D
 import qualified Penny.Lincoln.Bits.Qty as Q
 
 tests :: TF.Test
 tests = TF.testGroup "PennyTest.Penny.Lincoln.Bits.Qty"
-  []
+  [ testProperty "((x `add` y) `subt` y) == y" prop_addSubt ]
 
 -- | Generates very small positive Decimals.
 verySmallDecimal :: G.Gen D.Decimal
@@ -32,24 +33,34 @@ typicalDecimal = G.sized $ \s ->
   <$> G.choose (0, 5)
   <*> G.choose (1, 2 ^ s)
 
+-- | Generates 1. The exponent depends on the size parameter (but
+-- never exceeds 255.) The mantissa adjusts so the result is 1.
+one :: G.Gen D.Decimal
+one = G.sized $ \s -> do
+  e <- G.choose (0, min (fromIntegral s) 255)
+  return $ D.Decimal e (1 * 10 ^ (fromIntegral e))
+
 genQty
   :: (G.Gen D.Decimal)
-  -> Ex.ExceptionalT QC.Result Gen Q.Qty
+  -> Ex.ExceptionalT P.Result G.Gen Q.Qty
 genQty g = do
   d <- lift g
   case Q.newQty d of
-    Nothing -> Ex.throwT (QC.failed { QC.reason = r })
+    Nothing -> Ex.throwT (P.failed { P.reason = r })
       where
         r = "Qty generation failed from decimal " ++ show d
     Just q -> return q
 
-{-
 -- | > (x + y - y) == x
-prop_addSubt :: QC.Gen QC.Result
-prop_addSubt = do
-  x <- typicalDecimal
-  y <- typicalDecimal
-  case (x `Qty.add` y) `Qty.subt` y of
-    Nothing -> return QC.failed
-    Just r -> if r ==
--}
+ex_addSubt :: Ex.ExceptionalT P.Result G.Gen P.Result
+ex_addSubt = do
+  x <- genQty typicalDecimal
+  y <- genQty typicalDecimal
+  case (x `Q.add` y) `Q.subt` y of
+    Nothing -> return P.failed
+    Just r -> return $ P.liftBool (r == x)
+
+-- | > (x + y - y) == x
+prop_addSubt :: QC.Gen P.Result
+prop_addSubt = Ex.resolveT return ex_addSubt
+
