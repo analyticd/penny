d75da87fb275d942e1e4c3f53d069dd1b5c1d5b2
Omari Norman <omari@smileystation.com>
2012-11-10 08:37:16 -0500

    Add tests

--- tests/PennyTest/Penny/Copper/Commodity.hs
+++ tests/PennyTest/Penny/Copper/Commodity.hs
@@ -10,13 +10,32 @@ import qualified Test.QuickCheck.Gen as G
 import qualified Test.QuickCheck.Property as P
 import Test.QuickCheck.Gen (Gen)
 import qualified PennyTest.Penny.Copper.Util as TU
-import qualified Penny.Copper.Account as A
+import qualified Penny.Copper.Commodity as CC
 import qualified Penny.Lincoln as L
 import qualified Text.Parsec as Ps
+import Text.Parsec.Text (Parser)
 
 tests :: TF.Test
 tests = TF.testGroup "PennyTest.Penny.Copper.Commodity"
-  [
+  [ testProperty
+    "Parsing rendered quoted level 1 should give same thing"
+    test_parseQuotedLvl1
+
+  , testProperty
+    "Parsing rendered level 2 should give same thing"
+    test_parseLvl2
+
+  , testProperty
+    "Parsing rendered level 3 should give same thing"
+    test_parseLvl3
+
+  , testProperty
+    "Parsing rendered left-side commodity should give same thing"
+    test_parseLeftSide
+
+  , testProperty
+    "Parsing rendered right-side commodity should give same thing"
+    test_parseRightSide
   ]
 
 lvl1Char :: Gen Char
@@ -43,3 +62,70 @@ lvl3Char = G.oneof [TU.unicodeAll, TU.letter, return '$']
 
 lvl3Cmdty :: Gen L.Commodity
 lvl3Cmdty = (L.Commodity . X.pack) <$> G.listOf1 lvl3Char
+
+-- | Using the given generator and renderer, generate a commodity and
+-- render it. Returns the commodity and the rendered text. Fails if
+-- the render fails.
+genRender
+  :: Gen L.Commodity
+  -> (L.Commodity -> Maybe X.Text)
+  -> Ex.ExceptionalT P.Result Gen (L.Commodity, X.Text)
+genRender g r = do
+  c <- lift g
+  case r c of
+    Nothing -> Ex.throwT P.failed { P.reason = msg }
+      where msg = "could not render generated commodity. "
+                  ++ "Generated commodity: " ++ show c
+    Just x -> return (c, x)
+
+-- | Using the given generator, renderer, and parser, generate a
+-- commodity, render it, and parse it. Fails if the parse does not
+-- yield the same thing as the generated commodity.
+genRenderParse
+  :: Gen L.Commodity
+  -> (L.Commodity -> Maybe X.Text)
+  -> Parser L.Commodity
+  -> Gen P.Result
+genRenderParse g r p = Ex.resolveT return $ do
+  (c, t) <- genRender g r
+  let parsed = Ps.parse p "" t
+  c' <- case parsed of
+    Left _ -> Ex.throwT P.failed { P.reason = msg }
+      where msg = "could not parse commodity"
+    Right result -> return result
+  return $ if c' == c
+    then P.succeeded
+    else let msg = "parsed commodity is not equal "
+                   ++ "to original commodity"
+          in P.failed { P.reason = msg }
+
+
+-- | Parsing rendered quoted level 1 should give same thing
+test_parseQuotedLvl1 :: Gen P.Result
+test_parseQuotedLvl1 =
+  genRenderParse lvl1Cmdty CC.renderQuotedLvl1
+    CC.quotedLvl1Cmdty
+
+-- | Parsing rendered level 2 should give same thing
+test_parseLvl2 :: Gen P.Result
+test_parseLvl2 =
+  genRenderParse lvl2Cmdty CC.renderLvl2 CC.lvl2Cmdty
+
+-- | Parsing rendered level 3 should give same thing
+test_parseLvl3 :: Gen P.Result
+test_parseLvl3 =
+  genRenderParse lvl3Cmdty CC.renderLvl3 CC.lvl3Cmdty
+
+-- | Parsing rendered left-side commodity should give same thing
+test_parseLeftSide :: Gen P.Result
+test_parseLeftSide = genRenderParse g r CC.leftSideCmdty
+  where
+    g = G.oneof [lvl1Cmdty, lvl3Cmdty]
+    r c = CC.renderLvl3 c <|> CC.renderQuotedLvl1 c
+
+-- | Parsing rendered right-side commodity should give same thing
+test_parseRightSide :: Gen P.Result
+test_parseRightSide = genRenderParse g r CC.rightSideCmdty
+  where
+    g = G.oneof [lvl1Cmdty, lvl2Cmdty]
+    r c = CC.renderLvl2 c <|> CC.renderQuotedLvl1 c
