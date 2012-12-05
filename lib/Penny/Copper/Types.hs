0e356d4ffa796596c840b5fc4cf9663b92de375e
Omari Norman <omari@smileystation.com>
2012-12-04 21:45:50 -0500

    add mapLedger

--- lib/Penny/Copper/Types.hs
+++ lib/Penny/Copper/Types.hs
@@ -1,6 +1,9 @@
 module Penny.Copper.Types where
 
+import Control.Applicative (Applicative (pure))
+import Data.Functor ((<$>))
 import qualified Data.Text as X
+import qualified Data.Traversable as T
 import qualified Penny.Lincoln as L
 import qualified Data.Monoid as M
 
@@ -13,8 +16,43 @@ data Item = BlankLine
           | Transaction L.Transaction
           deriving Show
 
+mapItem
+  :: (Comment -> Comment)
+  -> (L.PricePoint -> L.PricePoint)
+  -> (L.Transaction -> L.Transaction)
+  -> Item
+  -> Item
+mapItem fc fp ft i = case i of
+  BlankLine -> BlankLine
+  IComment c -> IComment $ fc c
+  PricePoint p -> PricePoint $ fp p
+  Transaction t -> Transaction $ ft t
+
+mapItemA
+  :: Applicative a
+  => (Comment -> a Comment)
+  -> (L.PricePoint -> a L.PricePoint)
+  -> (L.Transaction -> a L.Transaction)
+  -> Item
+  -> a Item
+mapItemA fc fp ft i = case i of
+  BlankLine -> pure BlankLine
+  IComment c -> IComment <$> fc c
+  PricePoint p -> PricePoint <$> fp p
+  Transaction t -> Transaction <$> ft t
+
 newtype Ledger = Ledger { unLedger :: [Item] }
         deriving Show
+
+mapLedger :: (Item -> Item) -> Ledger -> Ledger
+mapLedger f (Ledger is) = Ledger $ map f is
+
+mapLedgerA
+  :: Applicative a
+  => (Item -> a Item)
+  -> Ledger
+  -> a Ledger
+mapLedgerA f (Ledger is) = Ledger <$> T.traverse f is
 
 instance M.Monoid Ledger where
   mempty = Ledger []
