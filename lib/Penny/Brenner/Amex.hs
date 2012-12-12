2ed52186b99c4517500e1988733a35fb148e1388
Omari Norman <omari@smileystation.com>
2012-12-12 13:01:37 -0500

    types update

--- lib/Penny/Brenner/Amex.hs
+++ lib/Penny/Brenner/Amex.hs
@@ -1,10 +1,18 @@
 -- | Amex - processing American Express downloaded data
-module Penny.Brenner.Amex where
+module Penny.Brenner.Amex
+  ( Card(..)
+  , Config(..)
+  , R.GroupSpecs(..)
+  , R.GroupSpec(..)
+  , amexMain
+  ) where
 
 import qualified Penny.Brenner.Amex.Types as Y
 import Data.Maybe (mapMaybe)
 import qualified Data.Text as X
 import qualified Penny.Lincoln as L
+import qualified Penny.Lincoln.Builders as Bd
+import qualified Penny.Copper.Render as R
 import qualified Penny.Brenner.Amex.Clear as C
 import qualified Penny.Brenner.Amex.Import as I
 import qualified Penny.Brenner.Amex.Merge as M
@@ -13,12 +21,13 @@ import qualified Control.Monad.Exception.Synchronous as Ex
 import System.Exit (exitFailure)
 import qualified System.IO as IO
 
-amexMain :: Y.Config -> IO ()
+amexMain :: Config -> IO ()
 amexMain cf = do
   as <- MA.getArgs
   pr <- MA.getProgName
-  let r = MA.modes globalOpts (preProcessor cf) whatMode as
-  processParseResult pr cf r
+  let cf' = convertConfig cf
+      r = MA.modes globalOpts (preProcessor cf') whatMode as
+  processParseResult pr cf' r
 
 data Arg
   = AHelp
@@ -81,7 +90,7 @@ processParseResult
 processParseResult pr cf ex =
   case ex of
     Ex.Exception err -> do
-      IO.hPutStrLn IO.stderr . show $ err
+      IO.hPutStr IO.stderr $ MA.formatError pr err
       exitFailure
     Ex.Success g -> processResult pr cf g
 
@@ -153,3 +162,51 @@ showCard n c =
   ++ label "Currency"
      (X.unpack . L.unCommodity . Y.unCurrency . Y.currency $ c)
 
+-- | Information to configure a single card account. 
+data Card = Card
+  { dbLocation :: String
+    -- ^ Path and filename to where the database is kept. You can use
+    -- an absolute or relative path (if it is relative, it will be
+    -- resolved relative to the current directory at runtime.)
+
+  , amexAcct :: String
+    -- ^ The account that you use in your Penny file to hold
+    -- transactions for this card. Separate each sub-account with
+    -- colons (as you do in the Penny file.)
+
+  , defaultAcct :: String
+    -- ^ When new transactions are created, one of the postings will
+    -- be in the amexAcct given above. The other posting will be in
+    -- this account.
+
+  , currency :: String
+    -- ^ The commodity for the currency of your card (e.g. @$@).
+
+  , groupSpecs :: R.GroupSpecs
+    -- ^ How to group digits when printing the resulting ledger. All
+    -- quantities (not just those affected by this program) will be
+    -- formatted using this specification.
+
+  } deriving Show
+
+convertCard :: Card -> Y.Card
+convertCard (Card db ax df cy gs) = Y.Card
+  { Y.dbLocation = Y.DbLocation db
+  , Y.amexAcct = Y.AmexAcct . Bd.account $ ax
+  , Y.defaultAcct = Y.DefaultAcct . Bd.account $ df
+  , Y.currency = Y.Currency . L.Commodity . X.pack $ cy
+  , Y.groupSpecs = gs
+  }
+
+data Config = Config
+  { defaultCard :: Maybe Card
+  , moreCards :: [(String, Card)]
+  } deriving Show
+
+convertConfig :: Config -> Y.Config
+convertConfig (Config d m) = Y.Config
+  { Y.defaultCard = fmap convertCard d
+  , Y.moreCards =
+      let f (n, c) = (Y.Name n, convertCard c)
+      in map f m
+  }
