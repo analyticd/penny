28016c81443abbd89fe1259a32dab81531fc9e36
Omari Norman <omari@smileystation.com>
2012-12-27 09:27:58 -0700

    remove old colors

--- lib/Penny/Cabin/Parsers.hs
+++ lib/Penny/Cabin/Parsers.hs
@@ -2,21 +2,8 @@
 
 module Penny.Cabin.Parsers where
 
-import qualified Penny.Cabin.Colors as Col
-import qualified Penny.Cabin.Colors.DarkBackground as DB
-import qualified Penny.Cabin.Colors.LightBackground as LB
 import qualified Penny.Cabin.Options as CO
 import qualified System.Console.MultiArg.Combinator as C
-
-
--- | Process an argument for the user's background color
--- choice. Returns an Exception with the bad string if the user
--- supplied a bad background name; Success otherwise.
-background :: C.OptSpec (Col.DrCrColors, Col.BaseColors)
-background = C.OptSpec ["background"] "" (C.ChoiceArg ls)
-  where
-    ls = [ ("light", (LB.drCrColors, LB.baseColors))
-         , ("dark", (DB.drCrColors, DB.baseColors)) ]
 
 
 zeroBalances :: [C.OptSpec CO.ShowZeroBalances]
