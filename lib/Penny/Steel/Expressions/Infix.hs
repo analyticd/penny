279b544878367aabc2cdef6daaa3d16a690cf28c
Omari Norman <omari@smileystation.com>
2013-02-26 22:01:12 -0500

    WIP

--- lib/Penny/Steel/Expressions/Infix.hs
+++ lib/Penny/Steel/Expressions/Infix.hs
@@ -1,1 +1,48 @@
 module Penny.Steel.Expressions.Infix where
+
+import qualified Penny.Steel.Expressions.RPN as R
+import qualified Control.Monad.Exception.Synchronous as Ex
+import qualified Data.Foldable as Fdbl
+
+data InfixToken a
+  = TokRPN (R.Token a)
+  | TokParen Paren
+
+data Paren = Open | Close
+
+processInfixToken
+  :: ([R.Operator], [R.Token a])
+  -> InfixToken a
+  -> Maybe ([R.Operator], [R.Token a])
+processInfixToken (os, ts) t = case t of
+  TokRPN tok -> return $ processRPNToken (os, t) tok
+  TokParen p -> processParen (os, t) p
+
+processRPNToken
+  :: ([R.Operator], [R.Token a])
+  -> R.Token a
+  -> ([R.Operator], [R.Token a])
+processRPNToken (os, ts) t = case t of
+  p@(R.TokOperand _ _) -> (os, p:ts)
+  R.TokOperator d -> case d of
+    R.OpNot -> (R.OpNot : os, ts)
+    R.OpAnd -> (R.OpAnd : os, ts)
+    R.OpOr ->
+      let (os', ts') = popper os ts
+      in (R.OpOr : os', ts')
+
+popper :: [R.Operator] -> [R.Token a] -> ([R.Operator], [R.Token a])
+popper os ts = case os of
+  [] -> (os, ts)
+  x:xs -> case x of
+    R.OpAnd -> let os' = xs
+                   ts' = R.TokOperator R.OpAnd : ts
+             in popper os' ts'
+    _ -> (os, ts)
+
+processParen
+  :: ([R.Operator], [R.Token a])
+  -> Paren
+  -> Maybe ([R.Operator], [R.Token a])
+processParen (os, ts) p = case p of
+  
--- lib/Penny/Steel/Expressions/RPN.hs
+++ lib/Penny/Steel/Expressions/RPN.hs
@@ -1,126 +1,88 @@
 -- | Postfix, or RPN, expression parsing.
 --
--- An RPN expression consists of operands and operators; a token is
--- either an operand or an operator. For example, in the expression @5
--- 4 +@, the @5@ and the @4@ are operands; the @+@ is an operator;
--- each of these three is a token.
-module Penny.Steel.Expressions.RPN (
-  Operand(..),
-  Operator(Unary, Binary),
-  Token(TokOperand, TokOperator),
-  process) where
+-- This module parses RPN expressions where the operands are
+-- predicates and the operators are one of @and@, @or@, or @not@,
+-- where @and@ and @or@ are binary and @not@ is unary.
+module Penny.Steel.Expressions.RPN where
 
 import qualified Control.Monad.Exception.Synchronous as Ex
 import qualified Data.Foldable as Fdbl
 
--- | An operand; for example, in the expression @5 4 +@, @5@ and @4@
--- are operands.
-data Operand a loc = Operand
-  { opOperand :: a
-  , opLocation :: loc
-  } deriving Show
+data Token a
+  = TokOperand String (a -> Bool)
+  -- ^ The String describes the operand, for use in error messages and
+  -- for printing a diagnostic tree.
+  | TokOperator Operator
 
-newtype StackVal a loc = StackVal
-  { stkOperand :: a
-  , stkLocation :: Maybe loc
-  } deriving Show
+data Operator
+  = OpAnd
+  | OpOr
+  | OpNot
 
--- | Operators; for example, in the expression @5 4 +@, @+@ is an
--- operator. Because this is RPN, there is no operator precedence.
-data Operator a =
-  Unary (a -> a)
-  -- ^ Unary operators take only one operand (for example, a factorial
-  -- operator).
-
-  | Binary (a -> a -> a)
-    -- ^ Binary operators take two operands (for example, an addition
-    -- operator).
-
-data OperatorWithLoc a loc = OperatorWithLoc
-  { otOperator :: Operator a
-  , otLocation :: loc
-  } deriving Show
-
-instance Show (Operator a) where
-  show (Unary _) = "<unary operator>"
-  show (Binary _) = "<binary operator>"
-
--- | A token is either an operator or an operand.
-data Token a =
-  TokOperand (Operand a)
-  | TokOperator (OperatorWithLoc a)
-  deriving Show
-
-
--- | Given an operator, and the stack of operands, process the
--- operator. When parsing an RPN expression, encountering an operator
--- at the front of the queue of tokens to be processed means that the
--- correct number of tokens (one, for a unary operator, or two, for a
--- binary operator) must be popped off the stack of operands. The
--- operator is then applied to the operands. For a binary operator,
--- the binary function is applied first to the operand that was lowest
--- on the stack, and then to the operand that was higher up on the
--- stack. The result of the operator is then pushed onto the top of
--- the stack.
---
--- This function fails if there were insufficient operands on the
--- stack to process the operator. If successful, returns the new
--- stack, with the processed operands removed and the result of the
--- operator pushed onto the top of the stack.
-processOperator ::
-  Operator a
-  -> [StackVal a]
-  -> Maybe ([StackVal a])
-processOperator t ds = case t of
-  (Unary f) -> case ds of
-    [] -> Nothing
-    (StackVal x _):xs -> return $ (StackVal (f x) Nothing) : xs
-  (Binary f) -> case ds of
-    [] -> Nothing
-    (StackVal x _):dss -> case dss of
-      (StackVal y _):dsss ->
-        return $ (StackVal (f y x) Nothing) : dsss
-      [] -> Nothing
-
--- | Adds an operand to the top of the stack.
-processOperand ::
-  Operand a
-  -> [StackVal a]
-  -> [StackVal a]
-processOperand (Operand a loc) rs = (StackVal a (Just loc)) : rs
+data Tree a
+  = EAnd (Tree a) (Tree a)
+  | EOr (Tree a) (Tree a)
+  | ENot (Tree a)
+  | EOperand String (a -> Bool)
 
 data Error a
-  = InsufficientOperands (OperatorWithLoc a)
+  = InsufficientStack Operator
   | EmptyStack
-  | FullStack [StackVal a]
+  | FullStack [Tree a]
 
--- | Processes the next token. Fails if the next token is an operator
--- and fails; otherwise, returns the new stack of operands.
-processToken ::
-  [StackVal a]
-  -> Token a
-  -> Ex.Exceptional (Error a) ([StackVal a])
-processToken s tok = case tok of
-  TokOperand d -> return (processOperand d s)
-  TokOperator t@(OperatorWithLoc op _) ->
-    Ex.fromMaybe (InsufficientOperands t)
-    $ processOperator op s
+pushOperand :: String -> (a -> Bool) -> [Tree a] -> [Tree a]
+pushOperand s p ts = EOperand s p : ts
 
--- | Processes an entire input sequence of RPN tokens.
-process
+pushOperator :: Operator -> [Tree a] -> Ex.Exceptional (Error a) [Tree a]
+pushOperator o ts = case o of
+  OpAnd -> case ts of
+    x:y:zs -> return $ EAnd y x : zs
+    _ -> Ex.throw $ InsufficientStack OpAnd
+  OpOr -> case ts of
+    x:y:zs -> return $ EOr y x : zs
+    _ -> Ex.throw $ InsufficientStack OpOr
+  OpNot -> case ts of
+    x:zs -> return $ ENot x : zs
+    _ -> Ex.throw $ InsufficientStack OpNot
+
+pushToken :: [Tree a] -> Token a -> Ex.Exceptional (Error a) [Tree a]
+pushToken ts t = case t of
+  TokOperand d p -> return $ pushOperand d p ts
+  TokOperator o -> pushOperator o ts
+
+pushTokens
   :: Fdbl.Foldable f
   => f (Token a)
-  -- ^ A stack of tokens, with the end of the expression being at the
-  -- tail of the list and the beginning being at the head.
-
-  -> Ex.Exceptional (Error a) a
-  -- ^ Fails if there is not exactly one operand remaining on the
-  -- stack at the end of the parse, or if at any time there are
-  -- insufficient operands on the stack to parse an
-  -- operator. Otherwise, succeeds and returns the result.
-process ls = do
-  os <- Fdbl.foldlM processToken [] ls
-  case os of
-    [] -> Ex.throw EmptyStack
-    (Operand x) : [] -> return x
+  -> Ex.Exceptional (Error a) (Tree a)
+pushTokens ts = do
+  trees <- Fdbl.foldlM pushToken [] ts
+  case trees of
+    [] -> Ex.throw $ EmptyStack
+    x:[] -> return x
     xs -> Ex.throw $ FullStack xs
+
+evalTree :: Tree a -> a -> Bool
+evalTree t a = case t of
+  EAnd x y -> evalTree x a && evalTree y a
+  EOr x y -> evalTree x a || evalTree y a
+  ENot x -> not $ evalTree x a
+  EOperand _ p -> p a
+
+
+showTree :: Tree a -> String
+showTree t = showTree' 0 t
+
+indentation :: Int
+indentation = 4
+
+indent :: Int -> String -> String
+indent i s = replicate (i * indentation) ' ' ++ s ++ "\n"
+
+showTree' :: Int -> Tree a -> String
+showTree' l t = case t of
+  EAnd x y -> indent l "and" ++ showTree' (l + 1) x
+                             ++ showTree' (l + 1) y
+  EOr x y -> indent l "or" ++ showTree' (l + 1) x
+                           ++ showTree' (l + 1) y
+  ENot x -> indent l "not" ++ showTree' (l + 1) x
+  EOperand s _ -> indent l s
