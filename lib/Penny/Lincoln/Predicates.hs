c0ec69aab4b189877cd0a8a5ef069b18ded48879
Omari Norman <omari@smileystation.com>
2013-02-28 18:45:47 -0500

    WIP

--- lib/Penny/Lincoln/Predicates.hs
+++ lib/Penny/Lincoln/Predicates.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | Functions that return a boolean based upon some criterion that
 -- matches something, often a PostFam. Useful when filtering
 -- Postings.
@@ -5,6 +7,7 @@ module Penny.Lincoln.Predicates where
 
 
 import Data.List (intersperse)
+import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as X
 import Data.Time (Day)
@@ -14,9 +17,126 @@ import qualified Penny.Lincoln.Family.Family as F
 import qualified Penny.Lincoln.Queries as Q
 import Penny.Lincoln.Transaction (PostFam)
 import qualified Penny.Lincoln.Transaction as T
+import qualified Text.Matchers as M
+
+-- | Predicates also come with a description, which is useful for
+-- giving diagnostics to the user.
+data Pdct = Pdct
+  { pdct :: PostFam -> Bool
+  , description :: Text
+  }
+
+type MakePdct = M.Matcher -> Pdct
+
+-- * Matching helpers
+match
+  :: HasText a
+  => Text
+  -- ^ Description of this field
+  -> (PostFam -> a)
+  -- ^ Function that returns the field being matched
+  -> M.Matcher
+  -> Pdct
+match t f m = Pdct pd desc
+  where
+    desc = makeDesc t m
+    pd = M.match m . text . f
+
+matchMaybe
+  :: HasText a
+  => Text
+  -- ^ Description of this field
+  -> (PostFam -> Maybe a)
+  -> M.Matcher
+  -> Pdct
+matchMaybe t f m = Pdct pd desc
+  where
+    desc = makeDesc t m
+    pd = maybe False (M.match m . text) . f
+
+makeDesc :: Text -> M.Matcher -> Text
+makeDesc t m
+  = "subject: " <> t
+  <> " matcher: " <> M.matchDesc m
+
+-- | Does the given matcher match any of the elements of the Texts in
+-- a HasTextList?
+matchAny
+  :: HasTextList a
+  => Text
+  -> (PostFam -> a)
+  -> M.Matcher
+  -> Pdct
+matchAny t f m = Pdct pd desc
+  where
+    desc = makeDesc ("any of the components of " <> t) m
+    pd = any (M.match m) . textList . f
+
+matchAnyMaybe
+  :: HasTextList a
+  => Text
+  -> (PostFam -> Maybe a)
+  -> M.Matcher
+  -> Pdct
+matchAnyMaybe t f m = Pdct pd desc
+  where
+    desc = makeDesc ("any of the components of " <> t) m
+    pd = maybe False (any (M.match m) . textList) . f
+
+-- | Does the given matcher match the text that is at the given
+-- element of a HasTextList? If the HasTextList does not have a
+-- sufficent number of elements to perform this test, returns False.
+matchLevel
+  :: HasTextList a
+  => Int
+  -> Text
+  -> (PostFam -> a)
+  -> M.Matcher
+  -> Pdct
+matchLevel l d f m = Pdct pd desc
+  where
+    desc = makeDesc ("level " <> X.pack (show l) <> " of " <> d) m
+    pd pf = let ts = textList (f pf)
+            in if l < 0 || l >= length ts
+               then False
+               else M.match m (ts !! l)
+
+matchMaybeLevel
+  :: HasTextList a
+  => Int
+  -> Text
+  -> (PostFam -> Maybe a)
+  -> M.Matcher
+  -> Pdct
+matchMaybeLevel l d f m = Pdct pd desc
+  where
+    desc = makeDesc ("level " <> X.pack (show l) <> " of " <> d) m
+    doMatch a = let ts = textList a
+                in if l < 0 || l >= length ts
+                   then False
+                   else M.match m (ts !! l)
+    pd = maybe False doMatch . f
+
+-- | Does the matcher match the text of the memo? Joins each line of
+-- the memo with a space.
+matchMemo
+  :: Text
+  -> (PostFam -> Maybe B.Memo)
+  -> M.Matcher
+  -> Pdct
+matchMemo t f m = Pdct pd desc
+  where
+    desc = makeDesc t m
+    pd = maybe False doMatch . f
+    doMatch = M.match m
+              . X.intercalate (X.singleton ' ')
+              . B.unMemo
+
+
 
 -- * Matching helpers
 
+{-
 match :: HasText a => (Text -> Bool) -> a -> Bool
 match f = f . text
 
@@ -24,8 +144,6 @@ matchMaybe :: HasText a => (Text -> Bool) -> Maybe a -> Bool
 matchMaybe f = maybe False (match f)
 
 
--- | Does the given matcher match any of the elements of the Texts in
--- a HasTextList?
 matchAny :: HasTextList a => (Text -> Bool) -> a -> Bool
 matchAny f = any f . textList
 
@@ -193,3 +311,4 @@ clonedPostings p1 p2 =
   && (T.pEntry p1 == T.pEntry p2)
   && (T.pMemo p1 == T.pMemo p2)
   && (T.pInferred p1 == T.pInferred p2)
+-}
