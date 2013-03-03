4f731e502b95837786c25cfcb2e00401c22fa032
Omari Norman <omari@smileystation.com>
2013-03-03 07:32:09 -0500

    wafffles in Predtree

--- lib/Penny/Steel/Predtree.hs
+++ lib/Penny/Steel/Predtree.hs
@@ -6,6 +6,11 @@ module Penny.Steel.Predtree
   ( Pdct(..)
   , always
   , never
+  , pdctAnd
+  , pdctOr
+  , pdctNot
+  , operand
+  , waffle
   , (&&&)
   , (|||)
   , Level
@@ -14,6 +19,7 @@ module Penny.Steel.Predtree
   , evaluate
   ) where
 
+import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import qualified Data.Text as X
 import Data.Monoid ((<>), mconcat)
@@ -25,8 +31,16 @@ data Pdct a = Pdct Label (Node a)
 
 data Node a
   = And [Pdct a]
+  -- ^ None of the Pdct in list may be Just False. An empty list or
+  -- list with only Nothing is Just True.
+
   | Or [Pdct a]
+  -- ^ At least one of the Pdct in the list must be Just True. An
+  -- empty lis or list with only Nothing is Just False.
+
   | Not (Pdct a)
+  -- ^ Just True is Just False and vice versa; Nothing remains Nothing.
+
   | Operand (a -> Maybe Bool)
 
 pdctAnd :: Text -> [Pdct a] -> Pdct a
@@ -38,23 +52,16 @@ pdctOr t = Pdct t . Or
 pdctNot :: Text -> Pdct a -> Pdct a
 pdctNot t = Pdct t . Not
 
+-- | Creates a new operand. The Pdct is Just True or Just False, never
+-- Nothing.
 operand :: Text -> (a -> Bool) -> Pdct a
-operand t = Pdct t . Operand
+operand t = Pdct t . Operand . fmap Just
 
 -- | Turns an existing Pdct to a waffling one. If the underlying
 -- predicate returns Just True, the new Pdct also returns Just
 -- True. Otherwise, the Pdct returns Nothing. (Waffles can't say no.)
+-- Has no effect on non-Operand Pdct.
 waffle :: Pdct a -> Pdct a
-waffle p@(Pdct l n) =
-  let calc f a = case f a of
-        Nothing -> Nothing
-        Just b -> if b then Just True else Nothing
-  in Pdct t $ case n of
-      And ps -> case eval
-
-ePdct :: a -> Pdct a -> Maybe Bool
-ePdct (Pdct t 
-
 waffle p@(Pdct l n) = case n of
   Operand f ->
     let f' a = case f a of
@@ -95,25 +102,28 @@ showPdct amt lvl (Pdct l pd) = case pd of
   Not t -> indent amt lvl l <> showPdct amt (lvl + 1) t
   Operand _ -> indent amt lvl l
 
-labelBool :: Text -> Bool -> Text
+labelBool :: Text -> Maybe Bool -> Text
 labelBool t b = trueFalse <> " " <> t
   where
-    trueFalse = if b then "[TRUE] " else "[FALSE]"
+    trueFalse = case b of
+      Nothing -> "[skip] "
+      Just bl -> if bl then "[TRUE] " else "[FALSE]"
 
 evaluate :: IndentAmt -> a -> Level -> Pdct a -> (Maybe Bool, Text)
 evaluate i a lvl (Pdct l pd) = case pd of
   And ps -> let (resBool, resTxt) = evalAnd i a (lvl + 1) ps
-                txt = indent i lvl (labelBool l resBool)
+                txt = indent i lvl (labelBool l (Just resBool))
                         <> resTxt
-            in (resBool, txt)
+            in (Just resBool, txt)
   Or ps -> let (resBool, resTxt) = evalOr i a (lvl + 1) ps
-               txt = indent i lvl (labelBool l resBool)
+               txt = indent i lvl (labelBool l (Just resBool))
                         <> resTxt
-           in (resBool, txt)
-  Not p -> let (resBool, resTxt) = evaluate i a (lvl + 1) p
-               txt = indent i lvl (labelBool l (not resBool))
-                        <> resTxt
-           in (resBool, txt)
+           in (Just resBool, txt)
+  Not p -> let (childMayBool, childTxt) = evaluate i a (lvl + 1) p
+               thisMayBool = fmap not childMayBool
+               txt = indent i lvl (labelBool l thisMayBool)
+                        <> childTxt
+           in (thisMayBool, txt)
   Operand p -> let res = p a
                in (res, indent i lvl (labelBool l res))
 
@@ -126,16 +136,18 @@ evalAnd i a l ts = (not foundFalse, txt)
       if fndFalse
       then (fndFalse, acc <> indent i l "(short circuit)")
       else let (res, cTxt) = evaluate i a l x
-           in go xs (not res, acc <> cTxt)
+               fndFalse' = maybe False not res
+           in go xs (fndFalse', acc <> cTxt)
 
 evalOr :: IndentAmt -> a -> Level -> [Pdct a] -> (Bool, Text)
-evalOr i a l ts = (found, txt)
+evalOr i a l ts = (foundTrue, txt)
   where
-    (found, txt) = go ts (False, X.empty)
+    (foundTrue, txt) = go ts (False, X.empty)
     go [] p = p
     go (x:xs) (fnd, acc) =
       if fnd
       then (fnd, acc <> indent i l "(short circuit)")
       else let (res, cTxt) = evaluate i a l x
-           in go xs (res, acc <> cTxt)
+               fnd' = fromMaybe False res
+           in go xs (fnd', acc <> cTxt)
 
