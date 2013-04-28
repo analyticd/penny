3d6f6b0b02b590cf9c9c7d6eca1478a901209c8f
Omari Norman <omari@smileystation.com>
2013-04-28 09:30:17 -0400

    OFX changes

--- lib/Penny/Brenner/OFX.hs
+++ lib/Penny/Brenner/OFX.hs
@@ -1,7 +1,7 @@
 -- | Parses any OFX 1.0-series file. Uses the parser from the ofx
 -- package.
 
-module Penny.Brenner.OFX (parser) where
+module Penny.Brenner.OFX (parser, DescSign(..), ParserFn) where
 
 import Control.Applicative
 import qualified Control.Monad.Exception.Synchronous as Ex
@@ -13,40 +13,44 @@ import qualified Penny.Brenner.Types as Y
 import qualified Text.Parsec as P
 
 
-parser :: (String, Y.FitFileLocation
-                   -> IO (Ex.Exceptional String [Y.Posting]))
-parser = (help, loadIncoming)
+type ParserFn
+  = Y.FitFileLocation
+  -> IO (Ex.Exceptional String [Y.Posting])
 
-help :: String
-help = unlines
-  [ "Parses any OFX 1.0-series file."
-  , "Also parses QFX files, which are OFX files with minor"
-  , "proprietary additions from Intuit."
-  , "Many banks provide QFX files; they are typically labeled"
-  , "as being for Quicken. Some banks also provide files"
-  , "for the now-discontinued Microsoft Money; these often are"
-  , "OFX files."
-  , "Geared to work with deposit and credit card accounts. Although"
-  , "your brokerage house might give you OFX files for your account"
-  , "there, if those work with this parser, consider it a happy accident."
-  , "This parser is intended for 1.0-series OFX files; 2.0 series files"
-  , "are a bit different and they may or may not work."
-  ]
+-- | Do positive amounts increase or decrease the balance of the
+-- account? According to the OFX spec, amounts should always be
+-- positive if (from the customer's perspective) they increase the
+-- balance of the account, but not all OFX providers conform to this.
+data DescSign
+  = PosIsIncrease
+  | PosIsDecrease
 
-loadIncoming :: Y.FitFileLocation
-                -> IO (Ex.Exceptional String [Y.Posting])
-loadIncoming (Y.FitFileLocation fn) = do
+parser
+  :: String
+  -- ^ Help string
+  -> DescSign
+  -> (String, ParserFn)
+parser help d = (help, loadIncoming d)
+
+loadIncoming
+  :: DescSign
+  -> Y.FitFileLocation
+  -> IO (Ex.Exceptional String [Y.Posting])
+loadIncoming d (Y.FitFileLocation fn) = do
   contents <- readFile fn
   return $
     ( Ex.mapException show
       . Ex.fromEither
       $ P.parse O.ofxFile fn contents )
     >>= O.transactions
-    >>= mapM txnToPosting
+    >>= mapM (txnToPosting d)
 
 
-txnToPosting :: O.Transaction -> Ex.Exceptional String Y.Posting
-txnToPosting t = Y.Posting
+txnToPosting
+  :: DescSign
+  -> O.Transaction
+  -> Ex.Exceptional String Y.Posting
+txnToPosting d t = Y.Posting
   <$> pure (Y.Date ( T.utctDay . T.zonedTimeToUTC
                    . O.txDTPOSTED $ t))
   <*> pure (Y.Desc X.empty)
@@ -55,12 +59,19 @@ txnToPosting t = Y.Posting
   <*> pure ( Y.Payee $ case O.txPayeeInfo t of
               Nothing -> X.empty
               Just ei -> case ei of
-                Left d -> X.pack d
+                Left x -> X.pack x
                 Right p -> X.pack . O.peNAME $ p )
   <*> pure (Y.FitId . X.pack . O.txFITID $ t)
   where
     amtStr = O.txTRNAMT t
-    incDec = if "-" `isPrefixOf` amtStr then Y.Decrease else Y.Increase
+    incDec =
+      if "-" `isPrefixOf` amtStr
+      then case d of
+            PosIsIncrease -> Y.Decrease
+            PosIsDecrease -> Y.Increase
+      else case d of
+            PosIsIncrease -> Y.Increase
+            PosIsDecrease -> Y.Decrease
     amt = case amtStr of
       [] -> Ex.throw "empty amount"
       x:xs -> let str = if x == '-' || x == '+' then xs else amtStr
