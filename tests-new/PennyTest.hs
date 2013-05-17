ac44c43cae66e06af9c84850fdc0b17dff2761b3
Omari Norman <omari@smileystation.com>
2013-05-17 15:58:24 -0400

    remove cruft

--- tests-new/PennyTest.hs
+++
@@ -1,9 +0,0 @@
-module PennyTest where
-
-import Test.Framework (Test)
-import Test.Framework.QuickCheck2 (testGroup)
-
-import qualified PennyTests.Lincoln as L
-
-tests :: Test
-tests = testGroup "PennyTest" [ L.tests ]
