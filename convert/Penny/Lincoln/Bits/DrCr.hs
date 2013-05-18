c75e58aff51020293bcd8daa52964c1e20735204
Omari Norman <omari@smileystation.com>
2013-05-18 07:05:54 -0400

    remove cruft

--- convert/Penny/Lincoln/Bits/DrCr.hs
+++
@@ -1,9 +0,0 @@
-module Penny.Lincoln.Bits.DrCr where
-
-data DrCr = Debit | Credit deriving (Eq, Show, Ord)
-
--- | Debit returns Credit; Credit returns Debit
-opposite :: DrCr -> DrCr
-opposite d = case d of
-  Debit -> Credit
-  Credit -> Debit
--- convert/Penny/Lincoln/Bits/Number.hs
+++
@@ -1,6 +0,0 @@
-module Penny.Lincoln.Bits.Number where
-
-import Penny.Lincoln.TextNonEmpty (TextNonEmpty)
-
-newtype Number = Number { unNumber :: TextNonEmpty }
-                 deriving (Eq, Show, Ord)
