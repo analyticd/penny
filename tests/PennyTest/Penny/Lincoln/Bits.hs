794ee5cac24488b3d02ff3db7b37feb144b1c2de
Omari Norman <omari@smileystation.com>
2013-05-18 07:05:01 -0400

    remove old tests

--- tests/PennyTest/Penny/Lincoln/Bits.hs
+++
@@ -1,8 +0,0 @@
-module PennyTest.Penny.Lincoln.Bits where
-
-import qualified Test.Framework as TF
-import qualified PennyTest.Penny.Lincoln.Bits.Qty as Q
-
-tests :: TF.Test
-tests = TF.testGroup "PennyTest.Penny.Lincoln.Bits"
-  [ Q.tests ]
