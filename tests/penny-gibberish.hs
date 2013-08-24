b6f0ba03c2095d704e15b6b0094a383e44fc0f1c
Omari Norman <omari@smileystation.com>
2013-08-24 14:08:47 -0400

    tests compile

--- tests/penny-gibberish.hs
+++ tests/penny-gibberish.hs
@@ -23,26 +23,15 @@ help pn = unlines
   , "  -c, --count INT"
   , "      Number of items (transactions, comments, prices, and"
   , "      blank lines, total) to output. (default: 100)"
-  , "  -l, --left GROUP_SPEC"
-  , "      Group left of the decimal point (default: none)"
-  , "  -r, --right GROUP_SPEC"
-  , "      Group right of the decimal point (default: none)"
-  , "      where GROUP_SPEC is:"
-  , "        none - no digit grouping"
-  , "        large - group if greater than 9,999 left of decimal,"
-  , "                or more than 4 decimal places right of decimal"
-  , "        all - group whenever there are at least 4 places"
   ]
 
 data Opts = Opts
   { optSize :: Int
   , optCount :: Int
-  , optLeft :: R.GroupSpec
-  , optRight :: R.GroupSpec
   } deriving Show
 
 defaultOpts :: Opts
-defaultOpts = Opts 5 100 R.NoGrouping R.NoGrouping
+defaultOpts = Opts 5 100
 
 options :: [MA.OptSpec (Opts -> Opts)]
 options =
@@ -57,20 +46,7 @@ options =
       if i < 1
         then Ex.throw (MA.ErrorMsg "non-positive count parameter")
         else return (\os -> os { optCount = i })
-
-  , MA.OptSpec ["left"] "l" . MA.ChoiceArg
-    . map (\(str, spec) -> (str, \os -> os { optLeft = spec }))
-    $ groupSpecs
-
-  , MA.OptSpec ["right"] "r" . MA.ChoiceArg
-    . map (\(str, spec) -> (str, \os -> os { optRight = spec }))
-    $ groupSpecs
   ]
-
-groupSpecs :: [(String, R.GroupSpec)]
-groupSpecs = [ ("none",  R.NoGrouping)
-             , ("large", R.GroupLarge)
-             , ("all",   R.GroupAll  ) ]
 
 posArg :: a -> Ex.Exceptional MA.InputError b
 posArg _ = Ex.throw (MA.ErrorMsg "no non-option arguments accepted")
@@ -88,8 +64,7 @@ main = do
            . fmap (map fst)
            . replicateM (optCount os)
            $ P.item
-      gs = R.GroupSpecs (optLeft os) (optRight os)
-      x = mapM (R.item gs) is
+      x = mapM (R.item Nothing) is
   case x of
     Nothing -> do
       IO.hPutStrLn IO.stderr $ pn ++ ": error: could not render ledger."
