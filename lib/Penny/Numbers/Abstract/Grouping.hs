169f7fa672cb823b99ff29e65650e829290b08fd
Omari Norman <omari@smileystation.com>
2014-07-25 14:45:59 -0400

    WIP

--- lib/Penny/Numbers/Abstract/Grouping.hs
+++ lib/Penny/Numbers/Abstract/Grouping.hs
@@ -1,13 +1,12 @@
 {-# LANGUAGE RankNTypes #-}
-module Penny.Numbers.Abstract.Grouping (group, ungroup) where
+module Penny.Numbers.Abstract.Grouping (group) where
 
+import Data.Sequence (Seq, ViewL(..), ViewR(..), (<|))
 import qualified Data.Sequence as S
-import Deka.Native
 import Penny.Numbers.Abstract.Unpolar
 import Penny.Numbers.Abstract.Aggregates
 import Penny.Numbers.Abstract.RadGroup
 import Data.Sums
-import Data.List.Split (chunksOf)
 
 -- | Groups digits.  Rules for digit grouping:
 --
@@ -33,46 +32,69 @@ groupNonZero
   -> UngroupedNonZero r
   -> Either (UngroupedUnpolar r) (GroupedUnpolar r)
 groupNonZero grp o@(UngroupedNonZero s3) = case s3 of
-  S3a (UNWhole nvdcs@(NovDecs _ ds))
-    | S.length ds < 4 -> noGroup
-    | otherwise -> Right (GroupedUnpolar (S2b gnz))
-    where
-      gnz = GroupedNonZero (S5a (groupNovDecs grp nvdcs))
+  S3a (UNWhole nds) -> case groupNovDecs grp nds of
+    Nothing -> noGroup
+    Just gr -> Right (GroupedUnpolar (S2b (GroupedNonZero (S5a gr))))
 
-  S3b (UNWholeRadix nvdcs@(NovDecs _ ds) rd mayRt)
-    | S.length ds < 4 -> noGroup
-    | otherwise -> Right (GroupedUnpolar (S2b gnz))
-    where
-      gnz = GroupedNonZero (S5b glr)
-      glr = MasunoGroupedLeftRad (groupNovDecs grp nvdcs) rd suf
-      suf = fmap (\x -> (x, S.empty)) mayRt
+  S3b (UNWholeRadix nd rdx mdd) -> case groupNovDecs grp nd of
+    Nothing -> noGroup
+    Just gr -> Right (GroupedUnpolar (S2b (GroupedNonZero (S5b glr))))
+      where
+        glr = masunoGroupedLeftRad gr rdx mdd
 
   S3c _ -> noGroup
   where
     noGroup = Left (UngroupedUnpolar (S2b o))
 
+masunoGroupedLeftRad
+  :: MasunoGroupedLeft r
+  -> Radix r
+  -> Maybe DecDecs
+  -> MasunoGroupedLeftRad r
+masunoGroupedLeftRad mgl rdx mdd =
+  MasunoGroupedLeftRad mgl rdx mayDD
+  where
+    mayDD = fmap (\dd -> (dd, S.empty)) mdd
+
 groupNovDecs
-  :: (forall a. a -> Group g a)
+  :: (forall a. a -> Group r a)
   -> NovDecs
-  -> MasunoGroupedLeft g
-groupNovDecs = undefined
+  -> Maybe (MasunoGroupedLeft r)
+groupNovDecs g (NovDecs nv decs) =
+  case S.viewl (groupsOf3 makeGroup decs) of
+    EmptyL -> Nothing
+    (DecDecs d1 ds1) :< r1 -> case S.viewl r1 of
+      EmptyL -> Nothing
+      gr1 :< r2 -> case S.viewl ds1 of
+        EmptyL -> msd (S.singleton d1)
+        d2 :< digsRest -> case S.viewl digsRest of
+          EmptyL -> msd (d1 <| d2 <| S.empty)
+          d3 :< digsRest' -> case S.viewl digsRest' of
+            EmptyL -> msd (d1 <| d2 <| d3 <| S.empty)
+            _ -> error "groupWhole: too many groups"
+        where
+          msd nds =
+            Just (MasunoGroupedLeft (NovDecs nv nds) (g gr1) (fmap g r2))
 
-ungroup
-  :: GroupedUnpolar r
-  -> UngroupedUnpolar r
-ungroup = undefined
+  where
+    makeGroup (a1, m1) = case m1 of
+      Nothing -> DecDecs a1 S.empty
+      Just (a2, m2) -> case m2 of
+        Nothing -> DecDecs a2 (S.singleton a1)
+        Just a3 -> DecDecs a3 (a2 <| a2 <| S.empty)
 
--- | Splits a list into groups of 3.  If it doesn't divide evenly,
--- parts at the front will be shorter.
 
-groupsOf3 :: [a] -> [[a]]
-groupsOf3 = map reverse . reverse . chunksOf 3 . reverse
-
--- | Makes sure the first list has no more than 2 items.
-
-headNoMoreThan2 :: [[a]] -> [[a]]
-headNoMoreThan2 xs = case xs of
-  [] -> []
-  a:as
-    | length a < 3 -> a:as
-    | otherwise -> [head a] : tail a : as
+groupsOf3
+  :: ((a, Maybe (a, Maybe a)) -> b)
+  -> Seq a
+  -> Seq b
+groupsOf3 f = go S.empty
+  where
+    go soFar sq = case S.viewr sq of
+      EmptyR -> soFar
+      r1 :> a1 -> case S.viewr r1 of
+        EmptyR -> f (a1, Nothing) <| soFar
+        r2 :> a2 -> case S.viewr r2 of
+          EmptyR -> f (a1, Just (a2, Nothing)) <| soFar
+          r3 :> a3 ->
+            go ((f (a1, Just (a2, Just a3))) <| soFar) r3
