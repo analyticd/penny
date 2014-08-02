b78bc08c9d74198ea4b169de7033be0994cc10c9
Omari Norman <omari@smileystation.com>
2014-08-02 14:14:17 -0400

    WIP

--- library/lib/Penny/Trio.hs
+++ library/lib/Penny/Trio.hs
@@ -5,11 +5,11 @@ import Penny.Numbers.Qty
 import Penny.Numbers.Abstract.Aggregates
 import Penny.Numbers.Abstract.RadGroup
 
--- | When building entries using the "Penny.Ents.Ents" module, you may
+-- | When building entries using the "Penny.Ents" module, you may
 -- specify any of the 'Side', 'Commodity', a signed abstract quantity,
 -- or an unsigned abstract quantity.  Which of these you specify,
 -- combined with the running balance of the postings in the
--- transaction, determines whether an Ent is created or an error
+-- 'Penny.Ents.Ents', determines whether an Ent is created or an error
 -- occurs.
 --
 -- The order of the postings is important because Penny keeps a
@@ -21,51 +21,73 @@ import Penny.Numbers.Abstract.RadGroup
 -- quantity; @C@ for a 'Commodity', @S@ for @Side@, @U@ for unsigned
 -- abstract quantity 'Z' for @NZGrouped@, 'C' for @Commodity@, and 'E'
 -- for @Empty@.
---
--- The first type parameter is a signed abstract type; the second type
--- parameter is an unsigned abstract type.
 
 data Trio
   = QC (Either (Polar Period Side) (Polar Comma Side))
        Commodity Arrangement
   -- ^ Specify a quantity, commodity, and how they are arranged, and a
   -- corresponding entry is always recorded.
+  --
+  -- Preconditions: none
+  --
+  -- Postconditions: the balance is appropriately affected.
 
   | Q (Either (Polar Period Side) (Polar Comma Side))
-  -- ^ Specify a quantity only.  If there is exactly one commodity in
-  -- the balance, use it to infer the missing commodity.
+  -- ^ Specify a quantity only.
+  --
+  -- Preconditions: there is exactly one commodity in the imbalances.
+  -- This commodity is selected to create the entry.
+  --
+  -- Postconditions: the balance is appropriately affected.
 
   | SC Side Commodity
-  -- ^ Specify the 'Side' and the 'Commodity'.  If there is a
-  -- commodity in the balance, and the commodity in the balance has a
-  -- side that is opposite the side specified here, then record an
-  -- entry that offsets the commodity in the balance.
+  -- ^ Specify the 'Side' and the 'Commodity'.
+  --
+  -- Preconditions: the imbalances contain the given commodity, and
+  -- its balance has a side opposite the side given here.
+  --
+  -- Postconditions: the given commodity is eliminated from the
+  -- imbalances.
 
   | S Side
-  -- ^ Specify a 'Side' only.  If there is one commodity in the
-  -- balance, and that commodity has a 'Side' opposite to the one
-  -- specified here, then record an entry that offests the commodity
-  -- in the balance.
+  -- ^ Specify a 'Side' only.
+  --
+  -- Preconditions: the imbalances contain exactly one commodity, and
+  -- its 'Side' is opposite to the one specified here.
+  --
+  -- Postconditions: the imbalances is empty.
 
   | UC (Either (Unpolar Period) (Unpolar Comma)) Commodity Arrangement
   -- ^ Specify an unsigned abstract quantity only and a 'Commodity'
-  -- and how they are arranged.  If the given 'Commodity' is present
-  -- in the balance, then offset the 'Commodity' by the unsigned
-  -- amount given.  You must also specify how the unsigned quantity
-  -- and the 'Commodity' are arranged.
+  -- and how they are arranged.
+  --
+  -- Preconditions: the imbalances contains the given commodity.
+  --
+  -- Postconditions: the given commodity in the imbalances either has
+  -- its absolute value reduced or it flips to the opposite side.
 
   | U (Either (Unpolar Period) (Unpolar Comma))
-  -- ^ Specify an unsigned quantity only.  If there is one commodity
-  -- in the balance, and the absolute value of its quantity is less
-  -- than the absolute value of the unsigned quantity, then offset the
-  -- commodity in the balance by the given unsigned quantity.
+  -- ^ Specify an unsigned quantity only.
+  --
+  -- Preconditions: the imbalances contains exactly one commodity, and
+  -- its absolute value is greater than or equal to than the given amount.
+  --
+  -- Postconditions: the given commodity in the imbalances is reduced
+  -- by the amount given.
 
   | C Commodity
-  -- ^ Specify a 'Commodity' only.  If the given commodity is in the
-  -- balance, then offset it.
+  -- ^ Specify a 'Commodity' only.
+  --
+  -- Preconditions: the given commodity is in the imbalances.
+  --
+  -- Postconditions: the given commodity is eliminated from the
+  -- imbalances.
 
   | E
-  -- ^ Specify nothing at all.  If there is one commodity in the
-  -- balance, then offset it.
+  -- ^ Specify nothing at all.
+  --
+  -- Preconditions: there is exactly one commodity in the imbalances.
+  --
+  -- Postconditions: the imbalances is empty.
 
   deriving (Eq, Ord, Show)
