8329622fd2ed71d73bbd8bde593e6dc066bd614a
Omari Norman <omari@smileystation.com>
2014-08-23 19:37:18 -0400

    WIP

--- library/lib/Penny/Copper/Render.hs
+++ library/lib/Penny/Copper/Render.hs
@@ -2,6 +2,7 @@
 module Penny.Copper.Render where
 
 import Penny.Numbers.Natural hiding (length)
+import Penny.Numbers.Abstract.Aggregates
 import Data.Maybe (fromMaybe)
 import Data.List (genericLength)
 import Control.Monad
@@ -55,20 +56,32 @@ instance Renderable Decem where
 -- | Renders a sequence of renderable things.  Does not necessarily
 -- obey the 'Renderable' laws, because the sequence might be empty.
 
-renderSequence
+renderSequence'
   :: (Renderable a, F.Foldable f, Functor f)
   => f a
   -> Text
-renderSequence = X.concat . F.toList . fmap render
+renderSequence' = X.concat . F.toList . fmap render
 
 -- | Parses a sequence of renderable things.
 
-parseSequence :: Renderable a => Parser (Seq a)
-parseSequence = fmap fromList $ many parser
+parseSequence' :: Renderable a => Parser (Seq a)
+parseSequence' = fmap fromList $ many parser
+
+renderSequence
+  :: (F.Foldable f, Functor f)
+  => (a -> Text)
+  -> f a
+  -> Text
+renderSequence f = X.concat . F.toList . fmap f
+
+parseSequence
+  :: Parser a
+  -> Parser (Seq a)
+parseSequence p = fmap fromList $ many p
 
 instance Renderable NovDecs where
-  render (NovDecs n ds) = render n <> renderSequence ds
-  parser = liftM2 NovDecs parser parseSequence
+  render (NovDecs n ds) = render n <> renderSequence' ds
+  parser = liftM2 NovDecs parser parseSequence'
 
 instance Renderable ZeroesNovDecs where
   render (ZeroesNovDecs z nd) = renderNonNegZeroes z
@@ -76,8 +89,8 @@ instance Renderable ZeroesNovDecs where
   parser = liftM2 ZeroesNovDecs parseNonNegZeroes parser
 
 instance Renderable DecDecs where
-  render (DecDecs d ds) = render d <> renderSequence ds
-  parser = liftM2 DecDecs parser parseSequence
+  render (DecDecs d ds) = render d <> renderSequence' ds
+  parser = liftM2 DecDecs parser parseSequence'
 
 
 instance Renderable Zeroes where
@@ -135,27 +148,50 @@ instance Renderable a => Renderable (Grouper a) where
     <|> (char '_' >> return Under)
     <|> fmap Unique parser
 
+rPeriod :: Radix Period -> Text
+rPeriod _ = X.singleton '.'
+
+pPeriod :: Parser (Radix Period)
+pPeriod = char '.' >> return radPeriod
+
+rComma :: Radix Comma -> Text
+rComma _ = X.singleton ','
+
+pComma :: Parser (Radix Comma)
+pComma = char ',' >> return radComma
+
 instance Renderable (Radix Period) where
-  render _ = X.singleton '.'
-  parser = char '.' >> return radPeriod
+  render = rPeriod
+  parser = pPeriod
 
 instance Renderable (Radix Comma) where
-  render _ = X.singleton ','
-  parser = char ',' >> return radComma
+  render = rComma
+  parser = pComma
+
+rGroup :: (Grouper g -> Text) -> (p -> Text) -> Group g p -> Text
+rGroup rg rp (Group g p) = rg g <> rp p
+
+pGroup :: Parser (Grouper g) -> Parser p -> Parser (Group g p)
+pGroup pg pp = liftM2 Group pg pp
 
 instance (Renderable r, Renderable b) => Renderable (Group r b) where
-  render (Group g p) = render g <> render p
-  parser = liftM2 Group parser parser
+  render = rGroup render render
+  parser = pGroup parser parser
+
+rUNWholeRadix :: (Radix r -> Text) -> UNWholeRadix r -> Text
+rUNWholeRadix rr (UNWholeRadix nd rdx md) =
+  render nd <> rr rdx <> maybe X.empty render md
+
+pUNWholeRadix :: Parser (Radix r) -> Parser (UNWholeRadix r)
+pUNWholeRadix pr = liftM3 UNWholeRadix parser pr (optionMaybe parser)
 
 instance Renderable (UNWholeRadix Period) where
-  render (UNWholeRadix nd rdx md) =
-    render nd <> render rdx <> maybe X.empty render md
-  parser = liftM3 UNWholeRadix parser parser (optionMaybe parser)
+  render = rUNWholeRadix render
+  parser = pUNWholeRadix parser
 
 instance Renderable (UNWholeRadix Comma) where
-  render (UNWholeRadix nd rdx md) =
-    render nd <> render rdx <> maybe X.empty render md
-  parser = liftM3 UNWholeRadix parser parser (optionMaybe parser)
+  render = rUNWholeRadix render
+  parser = pUNWholeRadix parser
 
 -- | HasZeroDigit does not follow 'Renderable' properties, as its
 -- parser succeeds on the empty input, and its renderer may produce no
@@ -169,126 +205,367 @@ parseZeroDigit :: Parser HasZeroDigit
 parseZeroDigit = fmap HasZeroDigit $ (char '0' >> return True)
                                      <|> return False
 
-instance Renderable (UNRadFrac Period) where
-  render (UNRadFrac z rd znd) = renderZeroDigit z
-    <> render rd <> render znd
+rUNRadFrac :: (Radix r -> Text) -> UNRadFrac r -> Text
+rUNRadFrac rr (UNRadFrac z rd znd) = renderZeroDigit z
+  <> rr rd <> render znd
 
-  parser = liftM3 UNRadFrac parseZeroDigit parser parser
+pUNRadFrac :: Parser (Radix r) -> Parser (UNRadFrac r)
+pUNRadFrac pr = liftM3 UNRadFrac parseZeroDigit pr parser
+
+instance Renderable (UNRadFrac Period) where
+  render = rUNRadFrac render
+  parser = pUNRadFrac parser
 
 instance Renderable (UNRadFrac Comma) where
-  render (UNRadFrac z rd znd) = renderZeroDigit z
-    <> render rd <> render znd
-
-  parser = liftM3 UNRadFrac parseZeroDigit parser parser
+  render = rUNRadFrac render
+  parser = pUNRadFrac parser
 
 instance Renderable UZZeroOnly where
   render _ = X.singleton '0'
   parser = char '0' >> return UZZeroOnly
 
-instance Renderable (UZTrailing Period) where
-  render (UZTrailing z r mz) = renderZeroDigit z <> render r <>
-    maybe X.empty render mz
+rUZTrailing :: (Radix r -> Text) -> UZTrailing r -> Text
+rUZTrailing rr (UZTrailing z r mz) =
+  renderZeroDigit z <> rr r <> maybe X.empty render mz
 
-  parser = liftM3 UZTrailing parseZeroDigit parser (optionMaybe parser)
+pUZTrailing :: Parser (Radix r) -> Parser (UZTrailing r)
+pUZTrailing pr = liftM3 UZTrailing parseZeroDigit pr (optionMaybe parser)
+
+instance Renderable (UZTrailing Period) where
+  render = rUZTrailing render
+  parser = pUZTrailing parser
 
 instance Renderable (UZTrailing Comma) where
-  render (UZTrailing z r mz) = renderZeroDigit z <> render r <>
-    maybe X.empty render mz
+  render = rUZTrailing render
+  parser = pUZTrailing parser
 
-  parser = liftM3 UZTrailing parseZeroDigit parser (optionMaybe parser)
 
+rGZ
+  :: (Radix r -> Text)
+  -> (Group r Zeroes -> Text)
+  -> GZ r
+  -> Text
+rGZ rr rg (GZ hz r z g1 gs) = renderZeroDigit hz <> rr r
+    <> render z <> rg g1 <> renderSequence rg gs
+
+pGZ
+  :: Parser (Radix r)
+  -> Parser (Group r Zeroes)
+  -> Parser (GZ r)
+pGZ pr pg = liftM5 GZ parseZeroDigit pr parser pg (parseSequence pg)
 
 instance Renderable (GZ Period) where
-  render (GZ hz r z g1 gs) = renderZeroDigit hz <> render r
-    <> render z <> render g1 <> renderSequence gs
-
-  parser = liftM5 GZ parseZeroDigit parser parser parser parseSequence
+  render = rGZ render render
+  parser = pGZ parser parser
 
 instance Renderable (GZ Comma) where
-  render (GZ hz r z g1 gs) = renderZeroDigit hz <> render r
-    <> render z <> render g1 <> renderSequence gs
+  render = rGZ render render
+  parser = pGZ parser parser
 
-  parser = liftM5 GZ parseZeroDigit parser parser parser parseSequence
+
+rMasunoGroupedLeft
+  :: (Group r DecDecs -> Text)
+  -> MasunoGroupedLeft r
+  -> Text
+rMasunoGroupedLeft rg (MasunoGroupedLeft nd g1 gs) =
+  render nd <> rg g1 <> renderSequence rg gs
+
+pMasunoGroupedLeft
+  :: Parser (Group r DecDecs)
+  -> Parser (MasunoGroupedLeft r)
+pMasunoGroupedLeft pg = liftM3 MasunoGroupedLeft parser pg
+  (parseSequence pg)
 
 
 instance Renderable (MasunoGroupedLeft Period) where
-  render (MasunoGroupedLeft nd g1 gs) = render nd <> render g1
-    <> renderSequence gs
-
-  parser = liftM3 MasunoGroupedLeft parser parser parseSequence
+  render = rMasunoGroupedLeft render
+  parser = pMasunoGroupedLeft parser
 
 instance Renderable (MasunoGroupedLeft Comma) where
-  render (MasunoGroupedLeft nd g1 gs) = render nd <> render g1
-    <> renderSequence gs
+  render = rMasunoGroupedLeft render
+  parser = pMasunoGroupedLeft parser
 
-  parser = liftM3 MasunoGroupedLeft parser parser parseSequence
+rMasunoGroupedLeftRad
+  :: (Radix r -> Text)
+  -> (Group r DecDecs -> Text)
+  -> MasunoGroupedLeftRad r
+  -> Text
+rMasunoGroupedLeftRad rr rg (MasunoGroupedLeftRad mgl r my) =
+    rMasunoGroupedLeft rg mgl <> rr r
+    <> maybe X.empty (\(dd, sq) -> render dd <> renderSequence rg sq) my
+
+pMasunoGroupedLeftRad
+  :: Parser (Radix r)
+  -> Parser (Group r DecDecs)
+  -> Parser (MasunoGroupedLeftRad r)
+pMasunoGroupedLeftRad pr pg = liftM3 MasunoGroupedLeftRad
+  (pMasunoGroupedLeft pg)
+  pr (optionMaybe (liftM2 (,) parser (parseSequence pg)))
 
 instance Renderable (MasunoGroupedLeftRad Period) where
-  render (MasunoGroupedLeftRad mgl r my) =
-    render mgl <> render r
-    <> maybe X.empty (\(dd, sq) -> render dd <> renderSequence sq) my
-
-  parser = liftM3 MasunoGroupedLeftRad parser parser
-    (optionMaybe (liftM2 (,) parser parseSequence))
+  render = rMasunoGroupedLeftRad render render
+  parser = pMasunoGroupedLeftRad parser parser
 
 instance Renderable (MasunoGroupedLeftRad Comma) where
-  render (MasunoGroupedLeftRad mgl r my) =
-    render mgl <> render r
-    <> maybe X.empty (\(dd, sq) -> render dd <> renderSequence sq) my
+  render = rMasunoGroupedLeftRad render render
+  parser = pMasunoGroupedLeftRad parser parser
 
-  parser = liftM3 MasunoGroupedLeftRad parser parser
-    (optionMaybe (liftM2 (,) parser parseSequence))
+rMasunoGroupedRight
+  :: (Radix r -> Text)
+  -> (Group r DecDecs -> Text)
+  -> MasunoGroupedRight r
+  -> Text
+rMasunoGroupedRight rr rg (MasunoGroupedRight nd rd dd g1 gs) =
+  render nd <> rr rd <> render dd <> rg g1
+    <> renderSequence rg gs
+
+pMasunoGroupedRight
+  :: Parser (Radix r)
+  -> Parser (Group r DecDecs)
+  -> Parser (MasunoGroupedRight r)
+pMasunoGroupedRight pr pg = liftM5 MasunoGroupedRight parser
+  pr parser pg (parseSequence pg)
 
 instance Renderable (MasunoGroupedRight Comma) where
-  render (MasunoGroupedRight nd rd dd g1 gs) =
-    render nd <> render rd <> render dd <> render g1 <> renderSequence gs
-
-  parser = liftM5 MasunoGroupedRight parser parser parser parser
-    parseSequence
+  render = rMasunoGroupedRight render render
+  parser = pMasunoGroupedRight parser parser
 
 instance Renderable (MasunoGroupedRight Period) where
-  render (MasunoGroupedRight nd rd dd g1 gs) =
-    render nd <> render rd <> render dd <> render g1 <> renderSequence gs
+  render = rMasunoGroupedRight render render
+  parser = pMasunoGroupedRight parser parser
 
-  parser = liftM5 MasunoGroupedRight parser parser parser parser
-    parseSequence
+rFracunoFirstGroupZ
+  :: (Radix r -> Text)
+  -> (Group r Zeroes -> Text)
+  -> (Group r ZeroesNovDecs -> Text)
+  -> (Group r DecDecs -> Text)
+  -> FracunoFirstGroupZ r
+  -> Text
+rFracunoFirstGroupZ rr rz rznd rd (FracunoFirstGroupZ hz rdx z gz g gs) =
+  renderZeroDigit hz <> rr rdx <> render z <> renderSequence rz gz
+    <> rznd g <> renderSequence rd gs
+
+pFracunoFirstGroupZ
+  :: Parser (Radix r)
+  -> Parser (Group r Zeroes)
+  -> Parser (Group r ZeroesNovDecs)
+  -> Parser (Group r DecDecs)
+  -> Parser (FracunoFirstGroupZ r)
+pFracunoFirstGroupZ pr pz pznd pd =
+  FracunoFirstGroupZ
+  `fmap` parseZeroDigit
+  `ap` pr `ap` parser `ap` parseSequence pz
+  `ap` pznd `ap` parseSequence pd
 
 instance Renderable (FracunoFirstGroupZ Period) where
-  render (FracunoFirstGroupZ hz rd z gz g gs) =
-    renderZeroDigit hz <> render rd <> render z <> renderSequence gz
-      <> render g <> renderSequence gs
-
-  parser = FracunoFirstGroupZ
-    `fmap` parseZeroDigit
-    `ap` parser `ap` parser `ap` parseSequence `ap` parser
-    `ap` parseSequence
+  render = rFracunoFirstGroupZ render render render render
+  parser = pFracunoFirstGroupZ parser parser parser parser
 
 instance Renderable (FracunoFirstGroupZ Comma) where
-  render (FracunoFirstGroupZ hz rd z gz g gs) =
-    renderZeroDigit hz <> render rd <> render z <> renderSequence gz
-      <> render g <> renderSequence gs
+  render = rFracunoFirstGroupZ render render render render
+  parser = pFracunoFirstGroupZ parser parser parser parser
 
-  parser = FracunoFirstGroupZ
-    `fmap` parseZeroDigit
-    `ap` parser `ap` parser `ap` parseSequence `ap` parser
-    `ap` parseSequence
+rFracunoFirstGroupNZ
+  :: (Radix r -> Text)
+  -> (Group r DecDecs -> Text)
+  -> FracunoFirstGroupNZ r
+  -> Text
+rFracunoFirstGroupNZ rr rd (FracunoFirstGroupNZ hz r znd g1 gs) =
+  renderZeroDigit hz <> rr r <> render znd <> rd g1
+    <> renderSequence rd gs
 
+pFracunoFirstGroupNZ
+  :: Parser (Radix r)
+  -> Parser (Group r DecDecs)
+  -> Parser (FracunoFirstGroupNZ r)
+pFracunoFirstGroupNZ pr pg =
+  liftM5 FracunoFirstGroupNZ parseZeroDigit pr parser
+      pg (parseSequence pg)
 
 instance Renderable (FracunoFirstGroupNZ Period) where
-  render (FracunoFirstGroupNZ hz r znd g1 gs) =
-    renderZeroDigit hz <> render r <> render znd <> render g1
-      <> renderSequence gs
-
-  parser = liftM5 FracunoFirstGroupNZ parseZeroDigit parser parser
-    parser parseSequence
+  render = rFracunoFirstGroupNZ render render
+  parser = pFracunoFirstGroupNZ parser parser
 
 instance Renderable (FracunoFirstGroupNZ Comma) where
-  render (FracunoFirstGroupNZ hz r znd g1 gs) =
-    renderZeroDigit hz <> render r <> render znd <> render g1
-      <> renderSequence gs
+  render = rFracunoFirstGroupNZ render render
+  parser = pFracunoFirstGroupNZ parser parser
 
-  parser = liftM5 FracunoFirstGroupNZ parseZeroDigit parser parser
-    parser parseSequence
 
 -- Aggregates
+
+rUngroupedZero
+  :: (Radix r -> Text)
+  -> UngroupedZero r
+  -> Text
+rUngroupedZero rr = caseS2 render (rUZTrailing rr)
+  . unUngroupedZero
+
+-- If there is NOT a zero digit, this must be a UZTrailing.  If
+-- there is a zero digit, see if we have a radix; if so, this is a
+-- UZTrailing; otherwise, it's a UZZeroOnly.
+pUngroupedZero
+  :: Parser (Radix r)
+  -> Parser (UngroupedZero r)
+pUngroupedZero pr = parseZeroDigit >>= f
+  where
+    f (HasZeroDigit z)
+      | z = do
+          mayRdx <- optionMaybe pr
+          fmap UngroupedZero $ case mayRdx of
+            Nothing -> return $ S2a UZZeroOnly
+            Just rdx -> do
+              zs <- optionMaybe parser
+              return . S2b $
+                UZTrailing (HasZeroDigit True) rdx zs
+
+      | otherwise = do
+          rdx <- pr
+          zs <- optionMaybe parser
+          return . UngroupedZero . S2b
+            $ UZTrailing (HasZeroDigit False) rdx zs
+
+
+instance Renderable (UngroupedZero Comma) where
+  render = rUngroupedZero render
+  parser = pUngroupedZero parser
+
+instance Renderable (UngroupedZero Period) where
+  render = rUngroupedZero render
+  parser = pUngroupedZero parser
+
+-- | Parse an 'UngroupedNonZero'.  Sees if the input text leads off
+-- with a 'NovDecs'.  If so, this is either a 'UNWhole' or a
+-- 'UNWholeRadix', which is determined by seeing whether a 'Radix'
+-- comes next.  If input text does not lead off with a 'NovDecs', this
+-- must be a 'UNRadFrac'.
+
+pUngroupedNonZero
+  :: Parser (Radix r)
+  -> Parser (UngroupedNonZero r)
+pUngroupedNonZero pr = do
+  mayNd <- optionMaybe parser
+  case mayNd of
+    Nothing -> fmap (UngroupedNonZero . S3c) (pUNRadFrac pr)
+    Just nd -> do
+      mayRdx <- optionMaybe pr
+      case mayRdx of
+        Nothing -> return . UngroupedNonZero . S3a . UNWhole $ nd
+        Just rdx -> do
+          dd <- optionMaybe parser
+          return . UngroupedNonZero . S3b $ UNWholeRadix nd rdx dd
+
+rUngroupedNonZero
+  :: (Radix r -> Text)
+  -> UngroupedNonZero r
+  -> Text
+rUngroupedNonZero rr =
+  caseS3 render (rUNWholeRadix rr) (rUNRadFrac rr)
+  . unUngroupedNonZero
+
+instance Renderable (UngroupedNonZero Period) where
+  render = rUngroupedNonZero render
+  parser = pUngroupedNonZero parser
+
+instance Renderable (UngroupedNonZero Comma) where
+  render = rUngroupedNonZero render
+  parser = pUngroupedNonZero parser
+
+rUngroupedUnpolar
+  :: (Radix r -> Text)
+  -> UngroupedUnpolar r
+  -> Text
+rUngroupedUnpolar rr =
+  caseS2 (rUngroupedZero rr) (rUngroupedNonZero rr)
+  . unUngroupedUnpolar
+
+-- | Parses an 'UngroupedUnpolar'.
+--
+-- If a 'NovDecs' is first, this is a
+-- 'UNWhole' or 'UNWholeRadix'; which is determined by whether a radix
+-- is next.
+--
+-- If a radix is first, this is a 'UNRadFrac' or a
+-- 'UZTrailing'; which is determined by whether there are any NovDecs
+-- following the radix.
+--
+-- If a zero is first, this is a 'UZZeroOnly', 'UZTrailing', or
+-- 'UNRadFrac'; which is determined first by whether a radix follows
+-- the zero (which narrows it down to 'UZTrailing' or 'UNRadFrac' if
+-- there is) and then whether there are any NovDecs following the
+-- radix (this is a 'UNRadFrac' if there are.)
+
+pUngroupedUnpolar :: Parser (Radix r) -> Parser (UngroupedUnpolar r)
+pUngroupedUnpolar pr = fmap UngroupedUnpolar $
+  fmap (S2b . UngroupedNonZero) (firstNovDecs pr)
+  <|> firstRadix pr
+  <|> firstZero pr
+
+-- | Parses either a 'UNWhole' or a 'UNWholeRadix' where the first set
+-- of characters is a 'NovDecs'.
+firstNovDecs
+  :: Parser (Radix r)
+  -> Parser (S3 UNWhole (UNWholeRadix r) a)
+firstNovDecs pr = do
+  nd <- parser
+  mayRdx <- optionMaybe pr
+  case mayRdx of
+    Nothing -> return . S3a $ UNWhole nd
+    Just rdx -> do
+      dd <- optionMaybe parser
+      return . S3b $ UNWholeRadix nd rdx dd
+
+
+-- | Parses either an 'UngroupedZero' or an 'UngroupedNonZero' where
+-- the first character is a radix point.
+firstRadix
+  :: Parser (Radix r)
+  -> Parser (S2 (UngroupedZero r) (UngroupedNonZero r))
+firstRadix pr = do
+  rdx <- pr
+  zs <- parseNonNegZeroes
+  mayNd <- optionMaybe parser
+  case mayNd of
+    Nothing -> return . S2a . UngroupedZero
+      . S2b $ UZTrailing hzd rdx mz
+      where
+        mz = fmap Zeroes . nonNegToPos $ zs
+
+    Just nd -> return . S2b . UngroupedNonZero
+      . S3c $ UNRadFrac hzd rdx (ZeroesNovDecs zs nd)
+  where
+    hzd = HasZeroDigit False
+
+-- | Parses either an 'UngroupedZero' or an 'UngroupedNonZero' where
+-- the first character is a zero.
+firstZero
+  :: Parser (Radix r)
+  -> Parser (S2 (UngroupedZero r) (UngroupedNonZero r))
+firstZero pr = do
+  _ <- char '0'
+  mayRdx <- optionMaybe pr
+  case mayRdx of
+    Nothing -> return . S2a . UngroupedZero . S2a $ UZZeroOnly
+    Just rdx -> do
+      mayZs <- optionMaybe parser
+      mayNd <- optionMaybe parser
+      case mayNd of
+        Nothing -> return . S2a . UngroupedZero
+          . S2b $ UZTrailing hzd rdx mayZs
+        Just nd -> return . S2b
+          . UngroupedNonZero . S3c $ UNRadFrac hzd
+          rdx (ZeroesNovDecs nnz nd)
+          where
+            nnz = case mayZs of
+              Nothing -> nonNegZero
+              Just (Zeroes ps) -> posToNonNeg ps
+  where
+    hzd = HasZeroDigit True
+
+instance Renderable (UngroupedUnpolar Comma) where
+  render = rUngroupedUnpolar render
+  parser = pUngroupedUnpolar parser
+
+instance Renderable (UngroupedUnpolar Period) where
+  render = rUngroupedUnpolar render
+  parser = pUngroupedUnpolar parser
 
