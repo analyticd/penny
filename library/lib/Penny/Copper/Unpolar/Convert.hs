1cea8c65d086034000551256bb67bee644c43ab5
Omari Norman <omari@smileystation.com>
2014-08-25 17:41:48 -0400

    WIP

--- library/lib/Penny/Copper/Unpolar/Convert.hs
+++ library/lib/Penny/Copper/Unpolar/Convert.hs
@@ -3,20 +3,21 @@
 module Penny.Copper.Unpolar.Convert where
 
 import Penny.Copper.Unpolar.Tree
-import Penny.Numbers.Abstract.Unpolar
+import Penny.Numbers.Abstract.Unpolar hiding (Zeroes)
 import Penny.Numbers.Abstract.Aggregates
 import Data.Sums
 import Penny.Numbers.Concrete
 import Penny.Numbers.Abstract.RadGroup
-import Data.Sequence (Seq)
+import Data.Sequence (Seq, (<|))
+import qualified Data.Sequence as S
 
-nDF1RadixDigits
+nDF1RadixDigitsFromNDF1Radix
   :: NovDecs
   -> Radix r
   -> DecDecs
   -> NDF1RadixDigits r
   -> Unpolar r
-nDF1RadixDigits nd rad dd ndf = case ndf of
+nDF1RadixDigitsFromNDF1Radix nd rad dd ndf = case ndf of
   NDF1RadixDigitsEnd ->
     Unpolar . S2a . UngroupedUnpolar . S2b . UngroupedNonZero
     . S3b $ UNWholeRadix nd rad (Just dd)
@@ -25,7 +26,6 @@ nDF1RadixDigits nd rad dd ndf = case ndf of
     Unpolar . S2b . GroupedUnpolar . S2b . GroupedNonZero . S5c
     $ MasunoGroupedRight nd rad dd g1 gs
 
--- | an 'NDF1Radix' occuring directly underneath an 'NDF1'.
 nDF1RadixFromNDF1
   :: NovDecs
   -> Radix r
@@ -35,23 +35,7 @@ nDF1RadixFromNDF1 nd rad ndf = case ndf of
   NDF1RadixEnd ->
     Unpolar . S2a . UngroupedUnpolar . S2b . UngroupedNonZero
     . S3b $ UNWholeRadix nd rad Nothing
-  NDF1RadixDigits dd rd -> nDF1RadixDigits nd rad dd rd
-
--- | an 'NDF1GroupRadix' occuring from an 'NDF1Group'.
-nDF1GroupRadixFromNDF1Group
-  :: NovDecs
-  -> Group r DecDecs
-  -> Seq (Group r DecDecs)
-  -> Radix r
-  -> NDF1Group r
-  -> Unpolar r
-nDF1GroupRadixFromNDF1Group nd g1 gs rd x = case x of
-  NDF1GroupEnd ->
-    Unpolar . S2b . GroupedUnpolar . S2b . GroupedNonZero
-    . S5b $ MasunoGroupedLeftRad mgl rd Nothing
-  NDF1GroupRa
-  where
-    mgl = MasunoGroupedLeft nd g1 gs
+  NDF1RadixDigits dd rd -> nDF1RadixDigitsFromNDF1Radix nd rad dd rd
 
 nDF1Group
   :: NovDecs
@@ -63,7 +47,40 @@ nDF1Group nd g1 gs x = case x of
   NDF1GroupEnd ->
     Unpolar . S2b . GroupedUnpolar . S2b . GroupedNonZero
     . S5a $ MasunoGroupedLeft nd g1 gs
-  NDF1GroupRadix rd ndfr -> undefined
+  NDF1GroupRadix rd ndfr -> nDF1RadixFromNDF1Group nd g1 gs rd ndfr
+
+nDF1RadixFromNDF1Group
+  :: NovDecs
+  -> Group r DecDecs
+  -> Seq (Group r DecDecs)
+  -> Radix r
+  -> NDF1Radix r
+  -> Unpolar r
+nDF1RadixFromNDF1Group nd g1 gs rd ndf1 = case ndf1 of
+  NDF1RadixEnd -> Unpolar . S2b . GroupedUnpolar . S2b . GroupedNonZero
+    . S5b $ MasunoGroupedLeftRad mgl rd Nothing
+  NDF1RadixDigits dd ndfRd ->
+    nDF1RadixDigitsFromNDF1Group nd g1 gs rd dd ndfRd
+  where
+    mgl = MasunoGroupedLeft nd g1 gs
+
+nDF1RadixDigitsFromNDF1Group
+  :: NovDecs
+  -> Group r DecDecs
+  -> Seq (Group r DecDecs)
+  -> Radix r
+  -> DecDecs
+  -> NDF1RadixDigits r
+  -> Unpolar r
+nDF1RadixDigitsFromNDF1Group nd g1 gs rd dd ndf =
+  Unpolar . S2b . GroupedUnpolar
+  . S2b . GroupedNonZero . S5b
+  $ MasunoGroupedLeftRad mgl rd (Just (dd, sq))
+  where
+    mgl = MasunoGroupedLeft nd g1 gs
+    sq = case ndf of
+      NDF1RadixDigitsEnd -> S.empty
+      NDF1RadixDigitsGroups gr1 grs -> (gr1 <| grs)
 
 nDF1
   :: NovDecs
@@ -73,4 +90,22 @@ nDF1 nd x = case x of
   NDF1End -> Unpolar . S2a . UngroupedUnpolar . S2b .
     UngroupedNonZero . S3a $ UNWhole nd
   NDF1Radix r ndfr -> nDF1RadixFromNDF1 nd r ndfr
-  _ -> undefined
+  NDF1Group g1 gs ndfg -> nDF1Group nd g1 gs ndfg
+
+novDecsFirst
+  :: NovDecsFirst r
+  -> Unpolar r
+novDecsFirst (NovDecsFirst nd ndf1) = nDF1 nd ndf1
+
+aRZNext
+  :: HasZeroDigit
+  -> Radix r
+  -> Zeroes
+  -> Group r Zeroes
+  -> Seq (Group r Zeroes)
+  -> ARZNext r
+  -> Unpolar r
+aRZNext hzd rdx zs g1 gs x = Unpolar . S2b . GroupedUnpolar $ case x of
+  ARZNEnd -> S2a $ GZ hzd rdx zs g1 gs
+  ARZNNovDecs nd dd -> S2b . GroupedNonZero $ FracunoFirstGroupZ
+    hzd rdx zs g1
