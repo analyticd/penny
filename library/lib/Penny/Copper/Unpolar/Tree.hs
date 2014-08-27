aa22a084086dbf5e8560283017accbbaf810333f
Omari Norman <omari@smileystation.com>
2014-08-26 22:22:30 -0400

    WIP

--- library/lib/Penny/Copper/Unpolar/Tree.hs
+++ library/lib/Penny/Copper/Unpolar/Tree.hs
@@ -6,117 +6,118 @@ import Data.Sums
 import Penny.Numbers.Abstract.RadGroup
 import Penny.Numbers.Concrete (NovDecs)
 import Penny.Numbers.Abstract.Unpolar hiding (Zeroes)
+import Deka.Native.Abstract
 
 -- | A single zero.
 data Zero = Zero
+  deriving (Eq, Ord, Show)
 
 -- | One or more zeroes.
 data Zeroes = Zeroes Zero (Seq Zero)
+  deriving (Eq, Ord, Show)
 
-newtype Start r = Start (S3 (NovDecsFirst r) (ZeroFirst r) (RadixFirst r))
+-- | A non-empty set that starts with something of one type and
+-- concludes with a list of items of a different type.
+data NE a b = NE a (Seq b)
+  deriving (Eq, Ord, Show)
 
--- | 'NovDecs' that occur on the left side of the string.  All these
--- values necessarily are not equal to zero.
-data NovDecsFirst r = NovDecsFirst NovDecs (NDF1 r)
+data Start r
+  = Masuno (NE Novem Decem) (Masuno1 r)
+  | LeadZero Zero (LZ1 r)
+  | LeadRadix (Radix r) (LR1 r)
+  deriving (Eq, Ord, Show)
 
--- | First production that occurs in the string after a
--- 'NovDecsFirst'.
+-- # Values greater than 1
 
-data NDF1 r
-  = NDF1End
-  -- ^ The string ends immediately after the 'NovDecs'.
-  | NDF1Radix (Radix r) (NDF1Radix r)
-  -- ^ The string is followed by a radix point.
-  | NDF1Group (Group r DecDecs) (Seq (Group r DecDecs)) (NDF1Group r)
-  -- ^ The string is followed by a group of digits
+data Masuno1 r
+  = Masuno1End
+  | Masuno1Radix (Radix r) (Masuno1Radix1 r)
+  deriving (Eq, Ord, Show)
 
--- | Occurs after the radix point in a string that begins (on the left
--- side of the radix) with a 'NovDecs'.
+data Masuno1Radix1 r
+  = Masuno1Radix1End
+  | Masuno1Radix1Digs (NE Decem Decem) (Seq (Group r (NE Decem Decem)))
+  deriving (Eq, Ord, Show)
 
-data NDF1Radix r
-  = NDF1RadixEnd
-  -- ^ The string ends immediately after the radix point.
-  | NDF1RadixDigits DecDecs (NDF1RadixDigits r)
-  -- ^ There are digits after the radix point.
+-- # Values leading with 0
 
--- | Digits occuring after the radix point in a string that begins (on
--- the left side of the radix) with a 'NovDecs'.  The first group of
--- digits after the radix point is already represented in the
--- 'NDF1Radix'.
-data NDF1RadixDigits r
-  = NDF1RadixDigitsEnd
-  -- ^ There are no groups after the first group.
-  | NDF1RadixDigitsGroups (Group r DecDecs) (Seq (Group r DecDecs))
-  -- ^ There is at least one group after the first group.
+data LZ1 r
+  = LZ1End
+  -- ^ End here.  The result is an ungrouped, zero value (a single zero.)
+  | LZ1Radix (Radix r) (LZ2 r)
+  -- ^ A radix point occurs.
+  deriving (Eq, Ord, Show)
 
--- | Immediately follows the first groups of digits in a string that
--- begins (left of the radix) with a 'NovDecs'.  There is at least one
--- group of digits to the left of this production, though there could
--- be more than one.
+-- | Inside of an LZ1Radix.  This is after the radix point.
+data LZ2 r
+  = LZ2End
+  -- ^ End here.  The result is an ungrouped, zero value (a single
+  -- zero, with a trailing radix point.)
+  | LZ2Zeroes (NE Zero Zero) (LZ3 r)
+  -- ^ Zeroes occur immediately after the radix point.  No groups yet.
+  | LZ2NonZero (NE Novem Decem) (LZ4 r)
+  -- ^ A non-zero value occurs immediately after the radix point.  No
+  -- groups yet.
+  deriving (Eq, Ord, Show)
 
-data NDF1Group r
-  = NDF1GroupEnd
-  -- ^ There is nothing after the first groups of digits.
-  | NDF1GroupRadix (Radix r) (NDF1Radix r)
-  -- ^ There is a radix point, possibly followed by something after
-  -- the radix.
+-- | Inside of an LZ2.  This is after the radix point, but so
+-- far all that has been seen are zeroes.
+data LZ3 r
+  = LZ3End
+  -- ^ End here.  The result is a zero value.
+  | LZ3NovDecs (NE Novem Decem) (LZ5 r)
+  -- ^ A 'Novem' is seen immediately after the sequence of zeroes.
+  -- The result is a non-zero value.  However, no groups have been
+  -- seen yet.
+  | LZ3Group (Group r (LZ6 r))
+  -- ^ A group occurs next.  It's not yet known whether this group is
+  -- a zero or non-zero group.
+  deriving (Eq, Ord, Show)
 
--- # ZeroFirst
+-- | Inside of an LZ2NonZero.  This is after the radix point, but
+-- there have been no groups yet.
+data LZ4 r
+  = LZ4End
+  -- ^ End here.  The result is a non-zero, ungrouped value.
+  | LZ4Groups (Group r (NE Decem Decem)) (Seq (Group r (NE Decem Decem)))
+  -- ^ Groups occur.  The result is a non-zero, grouped value.
+  deriving (Eq, Ord, Show)
 
--- | Strings that begin with a zero.
-data ZeroFirst r = ZeroFirst Zero (ZF r)
+-- | Inside of a LZ3NovDecs.  This is after the radix point, and a
+-- non-zero digit has already been seen, but no groups have been seen
+-- yet.
+data LZ5 r
+  = LZ5End
+  -- ^ End here.  The result is a non-zero, ungrouped value.
+  | LZ5Groups (Group r (NE Decem Decem)) (Seq (Group r (NE Decem Decem)))
+  -- ^ Groups occur.  The result is a non-zero, grouped value.
+  deriving (Eq, Ord, Show)
 
--- | First production that occurs after the zero in a 'ZeroFirst'.
+-- | Inside of an LZ3Group.  At this point, we know that the string
+-- has groups.  However, it might still contain all zeroes.
+data LZ6 r
+  = LZ6NovDecs (NE Novem Decem) (Seq (Group r (NE Decem Decem)))
+  -- ^ This group starts off with a non-zero value.  There might be
+  -- additional groups.  The result is a grouped, non-zero value.
+  | LZ6Zeroes (NE Zero Zero) (LZ7 r)
+  -- ^ This group starts off with zeroes.
+  deriving (Eq, Ord, Show)
 
-data ZF r
-  = ZFEnd
-  -- ^ There is nothing after the zero.
-  | ZFRadix (Radix r) (AfterRad r)
-  -- ^ There is a radix after the zero.
+-- | Inside of an 'LZ6Zeroes'.  The string has groups; however, it
+-- might still contain all zeroes.
+data LZ7 r
+  = LZ7End
+  -- ^ End here.  The result is a grouped, non-zero value.
+  | LZ7NovDecs (NE Novem Decem) (Seq (Group r (NE Decem Decem)))
+  -- ^ A 'Novem' appears.  There might be additional groups.  The
+  -- result is a grouped, non-zero value.
+  | LZ7Group (Group r (LZ6 r))
+  -- ^ An additional group appears.
+  deriving (Eq, Ord, Show)
 
--- | Production after the radix point, either in a 'ZeroFirst' or in a
--- 'RadixFirst'.
-data AfterRad r
-  = AREnd
-  -- ^ There is nothing after the radix point.
-  | ARZeroes Zeroes (ARZeroes r)
-  -- ^ There are one or more groups of zeroes after the radix point.
-  -- The first set of zeroes is not preceded by a grouping character.
-  | ARNovDecs NovDecs (Seq (Group r DecDecs))
-  -- ^ Immediately after the radix point is a NovDecs, followed by
-  -- optional groups.
+-- # Values leading with a radix point.  Reuses some of the LZ types.
 
--- | After the first set of zeroes after the radix.
-data ARZeroes r
-  = ARZEnd
-  -- ^ There is nothing after the first set of zeroes.
-  | ARZGroupsZ (Group r Zeroes) (Seq (Group r Zeroes)) (ARZNext r)
-  -- ^ There is at least one group of zeroes after the first set of
-  -- zeroes. FIXME - this is not predictive; after parsing a separator
-  -- in the last group, the entire parser will fail if a zero does not
-  -- follow, which is not desirable
-  | ARZNovDecs NovDecs (Seq (Group r DecDecs))
-  -- ^ Immediately after the first zeroes, without a grouping
-  -- character, is a 'NovDecs', followed by optional groups.
-
--- | After groups of zeroes to the right of the radix.
-data ARZNext r
-  = ARZNEnd
-  -- ^ There is nothing after the last group of zeroes.
-
-  | ARZNNovDecs NovDecs (Seq (Group r DecDecs))
-  -- ^ Immediately after the last group of zeroes (not separated by a
-  -- grouping character) there are NovDecs, followed by optional
-  -- groups of 'DecDecs'.
-
-  | ARZNGroups (Group r (Maybe Zeroes, NovDecs)) (Seq (Group r DecDecs))
-  -- ^ After the last group of zeroes, separated by a grouping
-  -- character, is a group that optionally leads with zeroes but then
-  -- contains at least one non-zero digit.  Optional additional groups
-  -- follow.
-
--- # RadixFirst
-
--- | The radix is the first character on the left of the string.
-data RadixFirst r = RadixFirst (Radix r) (AfterRad r)
-
+data LR1 r
+  = LR1Zero (NE Zero Zero) (LZ3 r)
+  | LR1NonZero (NE Novem Decem) (LZ4 r)
+  deriving (Eq, Ord, Show)
