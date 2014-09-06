2c683bb5e2ca0c95bc72bcc19365aaef8520d24b
Omari Norman <omari@smileystation.com>
2014-09-06 17:13:01 -0400

    WIP

--- library/lib/Penny/Copper/Convert/Collect.hs
+++ library/lib/Penny/Copper/Convert/Collect.hs
@@ -9,31 +9,45 @@ import Data.Sequence
 import qualified Data.Sequence as S
 import qualified Penny.Copper.Convert.Serialize as Z
 import Penny.Serial
+import qualified Penny.Common as C
 
+-- | Currently in a TopLineMemo
 newtype TopLineMemos = TopLineMemos
   { unTopLineMemos :: Seq (L.Located TM.Memo) }
   deriving (Eq, Ord, Show)
 
+addTopLineMemo :: TopLineMemos -> (L.Located TM.Memo) -> TopLineMemos
+addTopLineMemo (TopLineMemos s) p = TopLineMemos $ s |> p
+
+-- | Just saw a TopLine, waiting for a Posting
+data AfterTopLine = AfterTopLine
+  { atlMemos :: TopLineMemos
+  , atlTopLine :: L.Located T.TopLine
+  , atlTopLineGlobal :: Serial
+  , atlTopLineClxn :: Serial
+  } deriving (Eq, Ord, Show)
+
+data PostingBox = PostingBox
+  { pbPosting :: L.Located T.Posting
+  , pbGlobal :: Serial
+  , pbClxn :: Serial
+  , pbMemos :: Seq (L.Located PM.Memo)
+  } deriving (Eq, Ord, Show)
+
+-- | At least one posting has already been seen; currently
+-- processing additional postings or memo lines.
+data AfterPosting = AfterPosting
+  { apTopLine :: AfterTopLine
+  , apPostings :: Seq PostingBox
+  , apLast :: PostingBox
+  } deriving (Eq, Ord, Show)
+
 data State
-  = Clear
+  = S0
   -- ^ Not currently in posting or transaction; awaiting next input
-  | TopLineMemos (Seq (L.Located TM.Memo))
-  -- ^ Currently in a TopLineMemo
-  | AfterTopLine (Seq (L.Located TM.Memo))
-            (L.Located T.TopLine)
-            Serial
-            Serial
-  -- ^ Just saw a TopLine, waiting for a Posting
-  | AfterPosting (Seq (L.Located TM.Memo))
-                 ((L.Located T.TopLine), Serial, Serial)
-                 (Seq ( L.Located T.Posting
-                      , Serial
-                      , Serial
-                      , Seq (L.Located PM.Memo)))
-                 (L.Located T.Posting, Serial, Serial)
-                 (Seq (L.Located PM.Memo))
-  -- ^ At least one posting has already been seen; currently
-  -- processing additional postings
+  | S1 TopLineMemos
+  | S2 AfterTopLine
+  | S3 AfterPosting
   deriving (Eq, Ord, Show)
 
 data Error
@@ -42,18 +56,10 @@ data Error
   | PostingMemoWithoutPosting
   deriving (Eq, Ord, Show)
 
-data Packet = Packet
-  { pkTopLineMemo :: Seq (L.Located TM.Memo)
-  , pkTopLine :: (L.Located T.TopLine, Serial, Serial)
-  , pkPostings :: Seq ( L.Located T.Posting
-                      , Serial
-                      , Serial
-                      , Seq (L.Located PM.Memo))
-  } deriving (Eq, Ord, Show)
-
 data Good
   = NoOutput
-  | Output Packet
+  | OutTopLine AfterTopLine
+  | OutPostings AfterPosting
   deriving (Eq, Ord, Show)
 
 process
@@ -61,40 +67,167 @@ process
   -> State
   -> (State, Either (L.Located Error) Good)
 process (L.Located loc itm) st = case st of
-  Clear -> case itm of
-    Z.Item2TxnMemo m ->
-      ( TopLineMemos (singleton (L.Located loc m))
-                     , Right NoOutput)
-    Z.Item2TopLine tl s1 s2 ->
-      ( AfterTopLine S.empty (L.Located loc tl) s1 s2, Right NoOutput )
+  S0 -> procClear loc itm
+  S1 tlm -> procTopLineMemos tlm loc itm
+  S2 atl -> procAfterTopLine atl loc itm
+  S3 afp -> procAfterPosting afp loc itm
 
-    Z.Item2Posting _ _ _ ->
-      (Clear, Left (L.Located loc PostingWithoutTopLine))
+procClear
+  :: C.Location
+  -> Z.Item2
+  -> (State, Either (L.Located Error) Good)
+procClear loc itm = case itm of
+  Z.Item2TxnMemo m ->
+    ( S1 . TopLineMemos . singleton . L.Located loc $ m
+    , Right NoOutput)
+  Z.Item2TopLine tl s1 s2 ->
+    ( S2 $ AfterTopLine { atlMemos = TopLineMemos S.empty
+                        , atlTopLine = L.Located loc tl
+                        , atlTopLineGlobal = s1
+                        , atlTopLineClxn = s2 }
+    , Right NoOutput )
 
-    Z.Item2PstgMemo _ ->
-      (Clear, Left (L.Located loc PostingMemoWithoutPosting))
+  Z.Item2Posting _ _ _ ->
+    (S0, Left (L.Located loc PostingWithoutTopLine))
 
-  TopLineMemos sq -> case itm of
-    Z.Item2TxnMemo m ->
-      (TopLineMemos (sq |> L.Located loc m), Right NoOutput)
-    Z.Item2TopLine tl s1 s2 ->
-      ( AfterTopLine S.empty (L.Located loc tl) s1 s2
-      ,  Right NoOutput)
-    Z.Item2Posting _ _ _ ->
-      ( Clear, Left (L.Located loc PostingWithoutTopLine))
-    Z.Item2PstgMemo _ ->
-      ( Clear, Left (L.Located loc PostingMemoWithoutPosting))
+  Z.Item2PstgMemo _ ->
+    (S0, Left (L.Located loc PostingMemoWithoutPosting))
 
-  AfterTopLine ms tl s1 s2 -> case itm of
-    Z.Item2TxnMemo m ->
-      ( TopLineMemos (singleton (L.Located loc m)), out )
-    Z.Item2TopLine tl' s1' s2' ->
-      ( AfterTopLine S.empty (L.Located loc tl') s1' s2', out )
-    Z.Item2Posting p s1' s2' ->
-      ( AfterPosting ms (tl, s1, s2) S.empty
-                     (L.Located loc p, s1', s2') S.empty
-      , Right NoOutput)
-    Z.Item2PstgMemo _ ->
-      ( Clear, Left (L.Located loc PostingMemoWithoutPosting ))
+
+
+procTopLineMemos
+  :: TopLineMemos
+  -> C.Location
+  -> Z.Item2
+  -> (State, Either (L.Located Error) Good)
+procTopLineMemos tlm loc itm = case itm of
+  Z.Item2TxnMemo m ->
+    (S1 $ addTopLineMemo tlm (L.Located loc m), Right NoOutput)
+  Z.Item2TopLine tl s1 s2 ->
+    ( S2 $ AfterTopLine { atlMemos = tlm
+                        , atlTopLine = L.Located loc tl
+                        , atlTopLineGlobal = s1
+                        , atlTopLineClxn = s2
+                        }
+    ,  Right NoOutput)
+  Z.Item2Posting _ _ _ ->
+    ( S0, Left (L.Located loc PostingWithoutTopLine))
+  Z.Item2PstgMemo _ ->
+    ( S0, Left (L.Located loc PostingMemoWithoutPosting))
+
+
+procAfterTopLine
+  :: AfterTopLine
+  -> C.Location
+  -> Z.Item2
+  -> (State, Either (L.Located Error) Good)
+procAfterTopLine atl loc itm = case itm of
+  Z.Item2TxnMemo m ->
+    ( S1 $ TopLineMemos (singleton (L.Located loc m))
+    ,  Right $ OutTopLine atl )
+  Z.Item2TopLine tl' s1' s2' ->
+    ( S2 $ AfterTopLine { atlMemos = TopLineMemos S.empty
+                        , atlTopLine = L.Located loc tl'
+                        , atlTopLineGlobal = s1'
+                        , atlTopLineClxn = s2' }
+    , Right $ OutTopLine atl )
+  Z.Item2Posting p s1' s2' ->
+    ( S3 $ AfterPosting atl S.empty pb, Right NoOutput )
     where
-      out = Right (Output (Packet ms (tl, s1, s2) S.empty))
+      pb = PostingBox { pbPosting = L.Located loc p
+                      , pbGlobal = s1'
+                      , pbClxn = s2'
+                      , pbMemos = S.empty
+                      }
+  Z.Item2PstgMemo _ ->
+    ( S0, Left (L.Located loc PostingMemoWithoutPosting ))
+
+procAfterPosting
+  :: AfterPosting
+  -> C.Location
+  -> Z.Item2
+  -> (State, Either (L.Located Error) Good)
+procAfterPosting afp loc itm = case itm of
+  Z.Item2TxnMemo tm ->
+    ( S1 . TopLineMemos . S.singleton . L.Located loc $ tm
+    , Right . OutPostings $ afp )
+  Z.Item2TopLine tl s1 s2 ->
+    ( S2 $ AfterTopLine { atlMemos = TopLineMemos S.empty
+                        , atlTopLine = L.Located loc tl
+                        , atlTopLineGlobal = s1
+                        , atlTopLineClxn = s2 }
+    , Right . OutPostings $ afp )
+  Z.Item2Posting p s1 s2 ->
+    ( S3 $ AfterPosting { apTopLine = apTopLine afp
+                        , apPostings = apPostings afp |> apLast afp
+                        , apLast = PostingBox
+                          { pbPosting = L.Located loc p
+                          , pbGlobal = s1
+                          , pbClxn = s2
+                          , pbMemos = S.empty
+                          }
+                        }
+    , Right NoOutput )
+  Z.Item2PstgMemo m ->
+    ( S3 $ afp { apLast = (apLast afp)
+                { pbMemos = (pbMemos (apLast afp)) |>
+                  L.Located loc m } }
+    , Right NoOutput )
+
+destroy
+  :: State
+  -> Either Error Good
+destroy st = case st of
+  S0 -> Right NoOutput
+  S1 _ -> Left TopLineMemoWithoutTopLine
+  S2 atl -> Right $ OutTopLine atl
+  S3 app -> Right $ OutPostings app
+
+data Result = Result
+  { resClxn :: C.Clxn
+  , resErrors :: Seq (L.Located Error)
+  , resFinalError :: Maybe Error
+  , resGoods :: Seq (Either AfterTopLine AfterPosting)
+  } deriving (Eq, Ord, Show)
+
+runner
+  :: Seq (L.Located Z.Item2)
+  -> ( Maybe Error
+     , Seq (L.Located Error)
+     , Seq (Either AfterTopLine AfterPosting)
+     )
+runner items = (finalErr, errors, allGoods)
+  where
+    (stFin, errors, goods) = go items S0 S.empty S.empty
+    go sq st es gs = case viewl sq of
+      EmptyL -> (st, es, gs)
+      x :< xs -> go xs st' es' gs'
+        where
+          (st', ei) = process x st
+          (es', gs') = case ei of
+            Left err -> (es |> err, gs)
+            Right g -> (es, appendGood gs g)
+    (finalErr, allGoods) = case destroy stFin of
+      Left e -> (Just e, goods)
+      Right g -> (Nothing, appendGood goods g)
+
+appendGood
+  :: Seq (Either AfterTopLine AfterPosting)
+  -> Good
+  -> Seq (Either AfterTopLine AfterPosting)
+appendGood sq g = case g of
+  NoOutput -> sq
+  OutTopLine atl -> sq |> (Left atl)
+  OutPostings afp -> sq |> (Right afp)
+
+runPackage :: Z.Package2 -> Result
+runPackage (Z.Package2 cx sq) = Result cx es fin gs
+  where
+    (fin, es, gs) = runner sq
+
+newtype Results = Results { unResults :: Seq Result }
+  deriving (Eq, Ord, Show)
+
+
+collect :: Z.Packages2 -> Results
+collect = Results . fmap runPackage . Z.unPackages2
