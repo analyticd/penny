bb44d9b262111f3efa5fc205e22cd8b8d37c4f18
Omari Norman <omari@smileystation.com>
2014-09-08 17:51:06 -0400

    WIP

--- library/lib/Penny/Imbalances.hs
+++ library/lib/Penny/Imbalances.hs
@@ -1,32 +1,32 @@
 module Penny.Imbalances where
 
 import qualified Penny.Commodity as Cy
-import qualified Penny.Quant as Quant
 import qualified Data.Map as M
 import qualified Penny.Balances as Bal
 import qualified Penny.Qty as Qty
 import Data.Maybe
-import qualified Penny.Gravel as Gravel
 import qualified Penny.Concrete as Concrete
-import qualified Penny.Side as Side
+import qualified Penny.Quark as Quark
+import qualified Penny.Pebble as Pebble
 
-newtype T = T { toMap :: M.Map Cy.T (Quant.T Side.T) }
+newtype T = T { toMap :: M.Map Cy.T Quark.T }
   deriving (Eq, Ord, Show)
 
 fromBalances :: Bal.T -> T
 fromBalances = T . M.fromList . mapMaybe f . M.toList . Bal.toMap
   where
-    f (cy, qt) = case Quant.fromGravel
-      . Gravel.fromCement Side.fromSign
+    f (cy, qt) =
+      case Quark.fromPebble
+      . Pebble.fromCement
       . Concrete.toCement
       . Qty.toConcrete $ qt of
-      Nothing -> Nothing
-      Just quant -> Just (cy, quant)
+        Nothing -> Nothing
+        Just quant -> Just (cy, quant)
 
 toBalances :: T -> Bal.T
 toBalances = Bal.T . fmap f . toMap
   where
     f = Qty.fromConcrete
       . Concrete.fromCement
-      . Gravel.toCement Side.toSign
-      . Quant.toGravel
+      . Pebble.toCement
+      . Quark.toPebble
---
+++ library/lib/Penny/Quark.hs
@@ -0,0 +1,17 @@
+module Penny.Quark where
+
+import qualified Penny.Quant as Q
+import qualified Penny.Side as Side
+import qualified Penny.Pebble as Pebble
+
+newtype T = T { toQuant :: Q.T Side.T }
+  deriving (Eq, Ord, Show)
+
+fromQuant :: Q.T Side.T -> T
+fromQuant = T
+
+fromPebble :: Pebble.T -> Maybe T
+fromPebble = fmap T . Q.fromGravel . Pebble.toGravel
+
+toPebble :: T -> Pebble.T
+toPebble = Pebble.fromGravel . Q.toGravel . toQuant
