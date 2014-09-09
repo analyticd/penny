8b333f18c8c8a51541d5d42eec759482e765b027
Omari Norman <omari@smileystation.com>
2014-09-09 14:53:03 -0400

    WIP

--- library/lib/Penny/Qty.hs
+++ library/lib/Penny/Qty.hs
@@ -1,7 +1,6 @@
 module Penny.Qty where
 
 import qualified Penny.Concrete as Concrete
-import qualified Penny.Pebble as Pebble
 
 newtype T = T { toConcrete :: Concrete.T }
   deriving (Eq, Ord, Show)
