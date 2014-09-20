eded7e9a7566eda550c4cc246f5581bed21f7eaa
Omari Norman <omari@smileystation.com>
2014-09-19 21:49:22 -0400

    WIP

--- library/lib/Penny/Amount/Comma.hs
+++
@@ -1,1 +0,0 @@
-module Penny.Amount.Comma where
--- library/lib/Penny/Amount/Period.hs
+++
@@ -1,9 +0,0 @@
-module Penny.Amount.Period where
-
-import qualified Penny.Amount as Amount
-import qualified Penny.Core.Anna.RadPer as RadPer
-
--- | An Amount with a period as a radix.
-
-data T = T { toAmount :: Amount.T RadPer.T }
-  deriving (Eq, Ord, Show)
