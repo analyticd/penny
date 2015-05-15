6f4591aa79fc95259add6d5fbc7935949d03b2c7
Omari Norman <omari@smileystation.com>
2015-05-15 17:10:36 -0400

    WIP

--- lib/Penny/Commodity/Matcher.hs
+++ lib/Penny/Commodity/Matcher.hs
@@ -5,11 +5,11 @@ import Penny.Matcher
 import Data.Text (Text)
 import Penny.Commodity
 
-commodityName
+commodity
   :: Monad m
   => Matcher Text m a
   -> Matcher Commodity m a
-commodityName mtcr = do
+commodity mtcr = do
   inform "running Text matcher on commodity name"
   Commodity txt <- getSubject
   indent $ study mtcr txt
