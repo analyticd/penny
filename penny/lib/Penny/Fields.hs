79b307101261356b01bbddf8a1ca281a091f07a0
Omari Norman <omari@smileystation.com>
2016-05-29 16:30:12 -0400

    WIP

--- penny/lib/Penny/Fields.hs
+++ penny/lib/Penny/Fields.hs
@@ -3,6 +3,7 @@ module Penny.Fields where
 
 import qualified Control.Lens as Lens
 import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
 import Data.Text (Text)
 import Data.Time (ZonedTime)
 
@@ -13,6 +14,9 @@ data TopLineFields = TopLineFields
 
 Lens.makeLenses ''TopLineFields
 
+emptyTopLineFields :: ZonedTime -> TopLineFields
+emptyTopLineFields zt = TopLineFields zt Nothing
+
 data PostingFields = PostingFields
   { _number :: Maybe Integer
   , _flag :: Maybe Text
@@ -20,5 +24,9 @@ data PostingFields = PostingFields
   , _fitid :: Maybe Text
   , _tags :: Seq Text
   } deriving Show
+
+emptyPostingFields :: PostingFields
+emptyPostingFields = PostingFields Nothing Nothing (Seq.empty) Nothing
+  Seq.empty
 
 Lens.makeLenses ''PostingFields
--- penny/lib/Penny/Transaction.hs
+++ penny/lib/Penny/Transaction.hs
@@ -1,16 +1,63 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 module Penny.Transaction where
 
 import Penny.Ents
 import Penny.Fields
 import Penny.Tree
+import qualified Penny.Fields as F
 
 import qualified Control.Lens as Lens
 import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import Data.Text (Text)
+import Data.Time (ZonedTime)
 
-data Transaction = Transaction
-  { _topLine :: (TopLineFields, Seq Tree)
-  , _postings :: Balanced (PostingFields, Seq Tree)
+data TopLineOrPosting b a = TopLineOrPosting
+  { _location :: a
+  , _ancillary :: Seq Tree
+  , _fields :: b
+  } deriving (Functor, Foldable, Traversable)
+
+emptyTopLineOrPosting :: b -> TopLineOrPosting b ()
+emptyTopLineOrPosting = TopLineOrPosting () Seq.empty
+
+Lens.makeLenses ''TopLineOrPosting
+
+type Posting a = TopLineOrPosting F.PostingFields a
+type TopLine a = TopLineOrPosting F.TopLineFields a
+
+data Transaction a = Transaction
+  { _topLine :: TopLine a
+  , _postings :: Balanced (Posting a)
   }
 
 Lens.makeLenses ''Transaction
+
+emptyTopLine :: ZonedTime -> TopLine ()
+emptyTopLine zt = emptyTopLineOrPosting (F.emptyTopLineFields zt)
+
+emptyPosting :: Posting ()
+emptyPosting = emptyTopLineOrPosting F.emptyPostingFields
+
+date :: forall a. Lens.Lens' (TopLine a) ZonedTime
+date = fields . F.date
+
+payee :: forall a. Lens.Lens' (TopLine a) (Maybe Text)
+payee = fields . F.payee
+
+number :: forall a. Lens.Lens' (Posting a) (Maybe Integer)
+number = fields . F.number
+
+flag :: forall a . Lens.Lens' (Posting a) (Maybe Text)
+flag = fields . F.flag
+
+account :: forall a. Lens.Lens' (Posting a) (Seq Text)
+account = fields . F.account
+
+fitid :: forall a. Lens.Lens' (Posting a) (Maybe Text)
+fitid = fields . F.fitid
+
+tags :: forall a. Lens.Lens' (Posting a) (Seq Text)
+tags = fields . F.tags
