93847a29b62240777724f499fb034324e9d2d1b3
Omari Norman <omari@smileystation.com>
2016-11-28 11:00:09 -0500

    version bump

--- genPennyCabal/genPennyCabal.hs
+++ genPennyCabal/genPennyCabal.hs
@@ -5,7 +5,7 @@ import Common
 import Dependencies
 
 pennyVer :: [Word]
-pennyVer = [0,33,20161121,1]
+pennyVer = [0,33,20161121,2]
 
 pennyCopper :: Package
 pennyCopper = exactly "penny-copper" [0,1,20161119,0]
